68f8f943f7320d2dbf5dbd8f99f0f26c31fa6b8c
Andrew Kitchener <Andrew.Kitchener@tessella.com>
2022-07-13 15:10:33 +0100

    UI - further tidying

--- UI/components/map.tsx
+++ UI/components/map.tsx
@@ -6,7 +6,7 @@ import type { GeoJsonObject } from 'geojson';
 import geoJSON from '../public/GeoJSON/geoJSON.json';
 import { VectorPoint } from '../data_types/vector_point';
 const myIcon = (prev: any) => new Icon({
- iconUrl: "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGlkPSJwaW4iIHJvbGU9ImltZyIgaGVpZ2h0PSIzMCIgdmlld0JveD0iMCAwIDI0IDMwIiBmaWxsPSJ3aGl0ZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCjxwYXRoIGQ9Ik0xMiAwQzguODE3NCAwIDUuNzY1MTYgMS4yNjQyOCAzLjUxNDcyIDMuNTE0NzJDMS4yNjQyOCA1Ljc2NTE2IDAgOC44MTc0IDAgMTJDMCAxOC42MjcgMTIgMzAgMTIgMzBDMTIgMzAgMjQgMTguNjI3IDI0IDEyQzI0IDguODE3NCAyMi43MzU3IDUuNzY1MTYgMjAuNDg1MyAzLjUxNDcyQzE4LjIzNDggMS4yNjQyOCAxNS4xODI2IDAgMTIgMFpNMTkuNSAxMy41SDEzLjVWMTkuNUgxMC41VjEzLjVINC41VjEwLjVIMTAuNVY0LjVIMTMuNVYxMC41SDE5LjVWMTMuNVoiIGZpbGw9ImJyb3duIi8+DQo8L3N2Zz4NCg==",
+ iconUrl: '../public/marker.svg',
  iconSize: [prev,prev]
 });
 
