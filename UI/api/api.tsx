9d3c8f3451b8c1155d1bca2e81eafbdc5f8d6e38
Andrew Kitchener <Andrew.Kitchener@tessella.com>
2022-07-13 15:37:10 +0100

    Disabled caching in apollo, and local builds in docker compose

--- UI/api/api.tsx
+++ UI/api/api.tsx
@@ -1,8 +1,18 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+const defaultOptions: DefaultOptions = {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+    },
+  }
 
 const client = new ApolloClient({
     uri: "/api/graphql",
     cache: new InMemoryCache(),
+    defaultOptions: defaultOptions,
 });
 
 export default client;
