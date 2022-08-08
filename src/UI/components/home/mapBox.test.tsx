ae09282d4e96704e3df80ea58190119fa2fd9e1d
Andrew Kitchener <Andrew.Kitchener@tessella.com>
2022-08-08 11:24:38 +0100

    Added simple tests to the homepage components

---
+++ src/UI/components/home/mapBox.test.tsx
@@ -0,0 +1,9 @@
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import MapBox from './mapBox';
+
+describe('MapBox component', () => {
+  it('renders', () => {
+    render(<MapBox />);
+  });
+});
