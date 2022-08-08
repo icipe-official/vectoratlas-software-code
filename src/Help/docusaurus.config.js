2b4e4d954851af97966fc3124e4e1d65287e30d4
Andrew Kitchener <Andrew.Kitchener@tessella.com>
2022-08-08 15:50:35 +0100

    Added placeholder help docusaurus project

---
+++ src/Help/docusaurus.config.js
@@ -0,0 +1,60 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Vector Atlas help',
+  tagline: 'Vector Atlas help site',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/Animals-Mosquito-icon.png',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        }
+
+      }),
+    ],
+  ],
+  themeConfig:
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  ({
+    navbar: {
+      title: 'Vector Atlas Help',
+      logo: {
+        alt: 'Vector Atlas Logo',
+        src: 'img/vector-atlas-logo.svg',
+        height: '100px'
+      },
+    },
+  })
+};
+
+module.exports = config;
