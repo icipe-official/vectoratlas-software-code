0a1dc02e4b0625d77253cf3ddf2db7290da8b37e
Andrew Kitchener <Andrew.Kitchener@tessella.com>
2022-08-11 11:58:57 +0100

    Fixing linting on API

--- src/API/src/config/config.controller.ts
+++ src/API/src/config/config.controller.ts
@@ -4,13 +4,13 @@ import * as fs from 'fs';
 
 @Controller('config')
 export class ConfigController {
-    @Get('featureflags')
-    async getFeatureFlags(): Promise<{flag: string, on: boolean}[]> {
-      return featureFlags;
-    };
+  @Get('featureflags')
+  async getFeatureFlags(): Promise<{flag: string; on: boolean}[]> {
+    return featureFlags;
+  }
 
-    @Get('version')
-    async getVersion(): Promise<string> {
-      return fs.readFileSync(`${process.cwd()}/public/version.txt`, 'utf8');
-    };
+  @Get('version')
+  async getVersion(): Promise<string> {
+    return fs.readFileSync(`${process.cwd()}/public/version.txt`, 'utf8');
+  }
 }
