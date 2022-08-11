aeb6d90f7d45795c47538fdfaddf9e139083ca7e
Ray <Millward>
2022-08-11 11:21:35 +0100

    updated the variables to work with the prod environment

--- src/API/src/db/datasource.ts
+++ src/API/src/db/datasource.ts
@@ -5,11 +5,11 @@ import { join } from 'path';
 console.log(process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, process.env.PGPORT, process.env.DB_NAME)
 export const typeOrmModuleOptions: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: '127.0.0.1',
+  host: process.env.POSTGRES_HOST,
   port: parseInt(process.env.PGPORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.POSTGRES_DB,
   entities: [join(__dirname, '**', '*.entity.{ts,js}')],
   migrations: [
     'src/db/migrations/*.ts'
