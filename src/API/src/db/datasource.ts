09c21fc427a36367f36b638b69e2bbc66e2bfc62
Christopher Sandbach <Christopher.Sandbach@tessella.com>
2022-08-16 14:55:37 +0100

    eslint double to single quotes

--- src/API/src/db/datasource.ts
+++ src/API/src/db/datasource.ts
@@ -1,8 +1,7 @@
-import {TypeOrmModuleOptions} from "@nestjs/typeorm";
-import { DataSource } from "typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { join } from 'path';
 
-console.log(process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, process.env.PGPORT, process.env.DB_NAME)
 export const typeOrmModuleOptions: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
@@ -11,14 +10,11 @@ export const typeOrmModuleOptions: TypeOrmModuleOptions = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-  migrations: [
-    'src/db/migrations/*.ts'
-  ]
-}
+  migrations: ['src/db/migrations/*.ts'],
+};
 
 const OrmConfig: DataSource = new DataSource({
   ...typeOrmModuleOptions,
   type: 'postgres',
 });
 export default OrmConfig;
-
