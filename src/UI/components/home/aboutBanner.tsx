c57a69aef93585ed6b0c31db71a622891783a308
Christopher Sandbach <Christopher.Sandbach@tessella.com>
2022-08-16 11:57:51 +0100

    Text added to about banner as specified in ticket. Test remains unchanged

--- src/UI/components/home/aboutBanner.tsx
+++ src/UI/components/home/aboutBanner.tsx
@@ -1,20 +1,29 @@
 import { Paper, Typography, Divider, Box, Button, Grid } from "@mui/material";
 
-
 export default function AboutBanner() {
   return (
-    <Paper sx={{
-      display: 'flex',
-      justifyContent: "center",
-      alignItems: "center",
-    }}>
-      <Typography variant="h5" color='primary' m='20px'>
-        Some main tagline with a brief mission statement
+    <Paper
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h5" color="primary" m="20px">
+        Analyses-Ready Data and Spatial Models Specifically Tailored to Inform
+        Malaria Vector Control
       </Typography>
       <Divider orientation="vertical" flexItem />
       <Box>
-        <Typography variant="body1" m='20px' mb="0px">
-          Vector Atlas is a Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        <Typography variant="body1" m="20px" mb="0px">
+          The Vector Atlas brings together a new collaboration of partners
+          (icipe, University of Oxford, MAP, PAMCA, GBIF, VectorBase, IRMapper,
+          BMGF) in an initiative to build an online, open access repository to
+          hold and share our analyses-ready malaria vector occurrence,
+          bionomics, abundance, and insecticide resistance data. Our data will
+          be fully up to date and form the basis of a series of spatial models
+          specifically tailored to inform the control of mosquito vectors of
+          disease.
         </Typography>
         <Grid container justifyContent="flex-end">
           <Button variant="contained">Join Mailing List</Button>
