b5bd70de3a49d35e2799f35d4fbe50f5591bd3f7
Christopher Sandbach <Christopher.Sandbach@tessella.com>
2022-08-17 15:47:01 +0100

    Created basic dummy state with team data. Moving onto Contact Information section prior to testing of page

--- src/UI/components/about/aboutTeamPanel.tsx
+++ src/UI/components/about/aboutTeamPanel.tsx
@@ -1,24 +1,18 @@
-import { Paper, Typography, Divider, Box } from '@mui/material';
+import { Grid, Avatar, Box } from '@mui/material';
 
-export default function AboutContact() {
+export default function AboutContact({name, location, position, imageURL}:{name:string, location: string, position: string, imageURL: string}) {
   return (
-    <Paper
-      sx={{
-        display: 'flex',
-        flexDirection:'column',
-        justifyContent: 'justify-between',
-        alignItems: 'left',
-      }}
-    >
-      <Typography variant="h5" color="primary" mt="20px" pl="15px" mb="5px">
-        Contact
-      </Typography>
-      <Divider orientation="horizontal" flexItem />
-      <Box p="15px">
-        <Typography variant="body1" textAlign="justify">
-          Contact Information. Emails addresses, building address etc
-        </Typography>
+    <Grid container item sx={{width:1/3, justifyContent:'center'}}>
+      <Box sx={{display:'inline-flex',width:1,cursor:'pointer', padding:2, borderRadius:5, border:2,borderColor:'primary.main'}}>
+        <Grid container sx={{width:1/2, justifyContent:'center', alignItems:'center'}}>
+          <Avatar sx={{height:150, width:150}} alt={name} src={imageURL}/>
+        </Grid>
+        <Grid container sx={{display:'inline-flex',flexDirection:'column',width:1/2, justifyContent:'center'}}>
+          <Box sx={{fontWeight:'bold', color:'primary.main'}}>{name}</Box>
+          <Box sx={{fontWeight:'Medium',marginBottom:2}}>{location}</Box>
+          <Box sx={{fontSize:'12px'}}>{position}</Box>
+        </Grid>
       </Box>
-    </Paper>
+    </Grid>
   );
 }
