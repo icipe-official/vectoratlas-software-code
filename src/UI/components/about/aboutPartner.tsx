1c96950e08f89b08eeeaa395dc6f09f91d578b45
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-08-18 17:08:36 +0100

    Quickly reformatting partner section for easier testing and data injection

--- src/UI/components/about/aboutPartner.tsx
+++ src/UI/components/about/aboutPartner.tsx
@@ -4,8 +4,11 @@ import {
   Box,
   Grid
 } from '@mui/material';
+import {dummyPartners} from './dummyState';
+import AboutPartnerPanel from './aboutPartnerPanel';
 
 export default function AboutPartner() {
+  const partnerList = dummyPartners.partners
   return (
     <Paper
       sx={{
@@ -19,63 +22,10 @@ export default function AboutPartner() {
         Our Partners
       </Typography>
       <Box p='35px' sx={{width:1,}}>
-        <Grid container sx={{width:1,}} spacing={15} alignItems='center'>
-          <Grid container item sx={{width:1/3, justifyContent:'center'}}>
-            <a href='http://www.icipe.org/' target='_blank' rel="noreferrer">
-              <Box sx={{cursor:'pointer'}}>
-                <img src='http://www.icipe.org/sites/default/files/icipe-logo-web-280px.png' height='70' alt='icipe' ></img>
-              </Box>
-            </a>
-          </Grid>
-          <Grid container item sx={{width:1/3, justifyContent:'center'}}>
-            <a href='https://www.ox.ac.uk/' target='_blank' rel="noreferrer">
-              <Box sx={{cursor:'pointer'}}>
-                <img src='https://logodownload.org/wp-content/uploads/2020/12/university-of-oxford-logo.png' height='70' alt='University of Oxford' ></img>
-              </Box>
-            </a>
-          </Grid>
-          <Grid container item sx={{width:1/3, justifyContent:'center'}}>
-            <a href='https://malariaatlas.org/' target='_blank' rel="noreferrer">
-              <Box sx={{cursor:'pointer'}}>
-                <img src='https://malariaatlas.org/wp-content/themes/map-wordpress/dist/images/malaria_atlas_project_logo.png' height='70' alt='Malaria Atlas Project'></img>
-              </Box>
-            </a>
-          </Grid>
-          <Grid container item sx={{width:1/3, justifyContent:'center'}}>
-            <a href='https://www.pamca.org/en' target='_blank' rel="noreferrer">
-              <Box sx={{cursor:'pointer',backgroundColor:'black', padding:1}}>
-                <img src='https://www.pamca.org/themes/pamca/assets/front_end/img/logo-default-slim-dark-small.png' height='70' alt='PAMCA'></img>
-              </Box>
-            </a>
-          </Grid>
-          <Grid container item sx={{width:1/3, justifyContent:'center'}}>
-            <a href='https://www.gbif.org/' target='_blank' rel="noreferrer">
-              <Box sx={{cursor:'pointer'}}>
-                <img src='https://docs.gbif.org/style/logo.svg' height='70' alt='GBIF'></img>
-              </Box>
-            </a>
-          </Grid>
-          <Grid container item sx={{width:1/3, justifyContent:'center'}}>
-            <a href='https://vectorbase.org/vectorbase/app' target='_blank' rel="noreferrer">
-              <Box sx={{cursor:'pointer'}}>
-                <img src='https://vectorbase.org/a/images/VEuPathDB/Logos-color-text-web/vectorbase.png'  height='70' alt='VectorBase'></img>
-              </Box>
-            </a>
-          </Grid>
-          <Grid container item sx={{width:1/3, justifyContent:'center'}}>
-            <a href='https://www.irmapper.com/' target='_blank' rel="noreferrer">
-              <Box sx={{cursor:'pointer'}}>
-                <img src='https://www.irmapper.com/img/logo1.png'  height='70'  alt='IRMapper'></img>
-              </Box>
-            </a>
-          </Grid>
-          <Grid container item sx={{width:1/3, justifyContent:'center'}}>
-            <a href='https://www.gatesfoundation.org/' target='_blank' rel="noreferrer">
-              <Box sx={{cursor:'pointer'}}>
-                <img src='https://www.gatesfoundation.org/-/media/logos/logolg.svg'  height='70' alt='BMGF'></img>
-              </Box>
-            </a>
-          </Grid>
+        <Grid data-testid='partnerListContainer' container sx={{width:1,}} spacing={15} alignItems='center'>
+          { partnerList.map(partner => (
+            <AboutPartnerPanel key={partner.id} id ={partner.id}name={partner.name} homepage={partner.homepage} imageURL={partner.imageURL} />
+          ))}
         </Grid>
       </Box>
     </Paper>
--- src/UI/components/about/dummyState.js
+++ src/UI/components/about/dummyState.js
@@ -35,3 +35,29 @@ export const dummyContact = {
   ]
 };
 
+export const dummyPartners = {
+  partners: [
+    {
+      id:11, name:'icipe', homepage:'http://www.icipe.org/', imageURL:'http://www.icipe.org/sites/default/files/icipe-logo-web-280px.png'
+    },
+    {
+      id:12, name:'University of Oxford', homepage:'https://www.ox.ac.uk/', imageURL:'https://logodownload.org/wp-content/uploads/2020/12/university-of-oxford-logo.png'
+    },{
+      id:13, name:'Malaria Atlas Project', homepage:'https://malariaatlas.org/', imageURL:'https://malariaatlas.org/wp-content/themes/map-wordpress/dist/images/malaria_atlas_project_logo.png'
+    },
+    {
+      id:14, name:'PAMCA', homepage:'https://www.pamca.org/en', imageURL:'https://www.pamca.org/themes/pamca/assets/front_end/img/logo-default-slim-dark-small.png'
+    },
+    {
+      id:15, name:'GBIF', homepage:'https://www.gbif.org/', imageURL:'https://docs.gbif.org/style/logo.svg'
+    },
+    {
+      id:16, name:'VectorBase', homepage:'https://vectorbase.org/vectorbase/app', imageURL:'https://vectorbase.org/a/images/VEuPathDB/Logos-color-text-web/vectorbase.png'
+    },{
+      id:17, name:'IRMapper', homepage:'https://www.irmapper.com/', imageURL:'https://www.irmapper.com/img/logo1.png'
+    },
+    {
+      id:18, name:'BMGF', homepage:'https://www.gatesfoundation.org/', imageURL:'https://www.gatesfoundation.org/-/media/logos/logolg.svg'
+    }
+  ]
+};
