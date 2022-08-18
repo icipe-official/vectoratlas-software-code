7fd13da86bccc7549d73282863f2fc9f7740a04c
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-08-18 11:02:32 +0100

    Starting testing write up

--- src/UI/components/about/dummyState.js
+++ src/UI/components/about/dummyState.js
@@ -8,11 +8,11 @@ export const dummyTeam = {
       id:3, name:'Christine H. Garza', location:'Uganda', position:'Project Accounting Manager, Finance and Administration Department', imageURL:'https://images.rawpixel.com/image_1300/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1LzQ0NS1tY2tpbnNleS0wNDc3LXBhaV8xLmpwZw.jpg'
     },
     {
-      id:4, name:'Lan M. Medina', location:'Ethiopia', position:'General Accounting Manager', imageURL:'https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80'
+      id:4, name:'Bernard L. John', location:'Ethiopia', position:'General Accounting Manager', imageURL:'https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80'
     }, {
-      id:5, name:'Cynthia P. Craig', location:'Togo', position:'Principal Investigator, Research Predatory Arthropods', imageURL:'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
+      id:5, name:'Joi M. Delaney', location:'Togo', position:'Principal Investigator, Research Predatory Arthropods', imageURL:'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
     },{
-      id:6, name:'Christine H. Garza', location:'Rwanda', position:'Visitng Scientist', imageURL:'https://images.unsplash.com/photo-1611608822650-925c227ef4d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+      id:6, name:'Curtis R. Galvin', location:'Rwanda', position:'Visitng Scientist', imageURL:'https://images.unsplash.com/photo-1611608822650-925c227ef4d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
     }
   ]
 };
