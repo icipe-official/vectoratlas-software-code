7121c52f49426f583ef23b53c89ba682cdbf61de
Christopher Sandbach <Christopher.Sandbach@tessella.com>
2022-08-23 13:28:52 +0100

    Refactoring Code - Learning about bash functions

--- src/TileServer/getMapData.sh
+++ src/TileServer/getMapData.sh
@@ -1,41 +1,25 @@
 mkdir -p data
 mkdir -p ./data/geojson
 
-wget -O data/ne_10m_admin_0_countries.zip https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip
-wget -O ./data/ne_10m_land.zip https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_land.zip
-wget -O ./data/ne_10m_ocean.zip https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_ocean.zip
-wget -O ./data/ne_10m_rivers_lake_centerlines.zip https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_rivers_lake_centerlines.zip
-wget -O ./data/ne_10m_lakes.zip https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_lakes.zip
+get_naturalEarthData () {
+    wget -O data/$2.zip https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/$1/$2.zip
+    unzip -o ./data/$2.zip -d ./data/$2
+    rm -r data/$2.zip
+    cd ./data/$2
+    ogr2ogr -f GeoJSON $3.json $2.shp
+    mv $3.json ../geojson/$3.json
+    cd ../../
+    rm -r ./data/$2
+}
 
-unzip -o ./data/ne_10m_admin_0_countries.zip -d ./data/ne_10m_admin_0_countries
-unzip -o ./data/ne_10m_land.zip -d ./data/ne_10m_land
-unzip -o ./data/ne_10m_ocean.zip -d ./data/ne_10m_ocean
-unzip -o ./data/ne_10m_rivers_lake_centerlines.zip -d ./data/ne_10m_rivers_lake_centerlines
-unzip -o ./data/ne_10m_lakes.zip -d ./data/ne_10m_lakes
+get_naturalEarthData cultural ne_10m_admin_0_countries countries
+get_naturalEarthData physical ne_10m_land land
+get_naturalEarthData physical ne_10m_ocean oceans
+get_naturalEarthData physical ne_10m_rivers_lake_centerlines rivers_lakes
+get_naturalEarthData physical ne_10m_lakes lakes_reservoirs
 
-find . -name "*.zip" -type f -delete
-
-cd ./data/ne_10m_admin_0_countries
-ogr2ogr -f GeoJSON countries.json ne_10m_admin_0_countries.shp
-mv countries.json ../geojson/countries.json
-cd ../ne_10m_land
-ogr2ogr -f GeoJSON land.json ne_10m_land.shp
-mv land.json ../geojson/land.json
-cd ../ne_10m_ocean
-ogr2ogr -f GeoJSON ocean.json ne_10m_ocean.shp
-mv ocean.json ../geojson/ocean.json
-cd ../ne_10m_rivers_lake_centerlines
-ogr2ogr -f GeoJSON rivers_lakes.json ne_10m_rivers_lake_centerlines.shp
-mv rivers_lakes.json ../geojson/rivers_lakes.json
-cd ../ne_10m_lakes 
-ogr2ogr -f GeoJSON lakes_reservoirs.json ne_10m_lakes.shp
-mv lakes_reservoirs.json ../geojson/lakes_reservoirs.json
-cd ../
-rm -r ne_10m_* 
-cd ../
-cd data/geojson
-
-tippecanoe -zg -Z5 -o rivers-and-lakes.mbtiles --coalesce-densest-as-needed --extend-zooms-if-still-dropping rivers_lakes.json lakes_reservoirs.json
-tippecanoe -zg -o land-and-oceans.mbtiles --coalesce-densest-as-needed --extend-zooms-if-still-dropping countries.json land.json ocean.json
+cd ./data/geojson
+tippecanoe -zg -Z5 -o rivers-and-lakes.mbtiles --coalesce-densest-as-needed --extend-zooms-if-still-dropping rivers_lakes.json lakes_reservoirs.json --force
+tippecanoe -zg -o land-and-oceans.mbtiles --coalesce-densest-as-needed --extend-zooms-if-still-dropping countries.json land.json oceans.json --force
 tile-join -o world.mbtiles rivers-and-lakes.mbtiles land-and-oceans.mbtiles --force
-cd ..
+cd ../../
--- src/TileServer/installTools.sh
+++ src/TileServer/installTools.sh
@@ -1,14 +1,10 @@
-chmod +x ./installTools.sh
-chmod +x ./getMapData.sh
-sudo apt-get install unzip
-sudo apt-get update
-sudo apt-get install gdal-bin
-sudo apt-get install -y make
-sudo apt-get install g++
-sudo apt-get install libsqlite3-dev
-sudo apt-get install libz-dev
-sudo apt install libpcap-dev
-sudo git clone https://github.com/mapbox/tippecanoe.git
+# chmod +x ./installTools.sh // SMG
+# chmod +x ./getMapData.sh
+
+apt-get update
+apt-get install -y unzip gdal-bin make g++ libsqlite3-dev libz-dev libpcap-dev
+
+git clone https://github.com/mapbox/tippecanoe.git
 cd tippecanoe
-sudo make -j
-sudo make install
+make -j
+make install
