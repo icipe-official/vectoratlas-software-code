20008de0425cb4b9207f5294327f57102b21f596
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-08-24 10:16:00 +0100

    All data changed to json

--- src/UI/components/about/aboutPartner.tsx
+++ src/UI/components/about/aboutPartner.tsx
@@ -1,9 +1,9 @@
 import { Box, Grid } from '@mui/material';
-import { partners } from './data/partners';
+import data from './data/partners.json';
 import AboutPartnerPanel from './aboutPartnerPanel';
 
 export default function AboutPartner() {
-  const allPartners = partners.partnerList;
+  const allPartners = data.partnerList;
   return (
     <Box p='35px' sx={{ width: 1 }}>
       <Grid data-testid='partnerListContainer' container sx={{ width: 1 }} spacing={15} alignItems='center'>
--- src/UI/components/about/aboutTeam.tsx
+++ src/UI/components/about/aboutTeam.tsx
@@ -1,9 +1,9 @@
 import { Box, Grid } from '@mui/material';
-import { team } from './data/team';
+import data from './data/team.json';
 import AboutTeamPanel from './aboutTeamPanel';
 
 export default function AboutTeam() {
-  const teamMembers = team.teamList;
+  const teamMembers = data.teamList;
   return (
     <Box p='35px' sx={{ width: 1 }}>
       <Grid data-testid='teamListContainer' container spacing={8} alignItems='center' justifyContent='center'>
