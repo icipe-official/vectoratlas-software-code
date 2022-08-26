ea6018aa6047650af98e744a08f1ad50e44b446e
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-08-26 10:03:35 +0100

    Changing apostrophes

--- src/UI/components/about/aboutHeader.tsx
+++ src/UI/components/about/aboutHeader.tsx
@@ -5,13 +5,13 @@ export default function AboutHeader() {
   return (
     <Box>
       <Typography variant='body1' textAlign='center'>
-        Maps are a powerful tool. They can illustrate the distribution of mosquito vector species known to transmit some of the world's most 
+        Maps are a powerful tool. They can illustrate the distribution of mosquito vector species known to transmit some of the world&apos;s most 
         debilitating diseases and highlight where these species are no longer susceptible to the insecticides used as their primary method of control. 
         All evidence-based maps rely on field data collected in a myriad of different ways by multiple data collectors for a wide variety of purposes. In 
         isolation, these data are able to answer the questions they were collected to address, but when combined, their value multiplies. The Vector Atlas is 
-        building a data-hub that links 'core' African vector occurrence, bionomics and insecticide resistance data to provide a  'one stop shop' of relatable 
-        and cross-referenced data access. Using our standardised and comprehensive collation protocols (Hay et al 2010) to abstract data from the published and 
-        grey literature (maintaining all data ownership), we are updating our core datasets (Sinka et al (2010), Massey et al (2016), Moyes et al (2020)) which 
+        building a data-hub that links &apos;core&apos; African vector occurrence, bionomics and insecticide resistance data to provide a  &apos;one stop shop&apos;
+        of relatable and cross-referenced data access. Using our standardised and comprehensive collation protocols (Hay et al 2010) to abstract data from the published 
+        and grey literature (maintaining all data ownership), we are updating our core datasets (Sinka et al (2010), Massey et al (2016), Moyes et al (2020)) which 
         will expand beyond the dominant vector species to cover the prominent secondary vector species responsible for residual transmission. We are adding 
         locally relevant human community behaviour, local flora (nectar sources) and fauna (livestock). Combined, these data will inform novel spatial models 
         and maps that promote and evolve our understanding of the variables that drive malaria transmission. 
