9eded6350a6c2043c2c829a1bd216281cc972329
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-09-02 12:10:13 +0100

    Testing and adding demo for tickets here as this represents natural progression of prior stories

--- src/API/src/config/config.controller.spec.ts
+++ src/API/src/config/config.controller.spec.ts
@@ -1,8 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigController } from './config.controller';
+import  * as featureFlags  from '../../public/feature_flags.json'
+import  * as mapStyles  from '../../public/map_styles.json'
 
 describe('ConfigController', () => {
   let controller: ConfigController;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,4 +18,18 @@ describe('ConfigController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('getFeatureFlags', () => {
+    it('should return a list of objects indicating the flagged components and their status', () => {
+      async function jestFlags(){
+        let jestFlagVals = await controller.getFeatureFlags();
+        console.log(jestFlagVals)
+        return jestFlagVals
+      }  
+      expect(jestFlags() === featureFlags)
+    });
+  })
 });
+
+
+// https://circleci.com/blog/getting-started-with-nestjs-and-automatic-testing/#c-consent-modal
