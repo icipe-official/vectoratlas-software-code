155e4fd6ccd17ae72c069b3601614c186ce70ba2
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-09-09 15:51:15 +0100

    Opacity slider - functional but slider itself buggy

--- src/UI/components/map.tsx
+++ src/UI/components/map.tsx
@@ -39,42 +39,61 @@ export const MapWrapper= () => {
   const mapElement =useRef(null);
 
   useEffect(() => {
+
+    const an_gambiae = new TileLayer({
+      source: new XYZ({
+        url: '/data/an_gambiae/{z}/{x}/{y}.png',
+        maxZoom: 5,
+      }),
+      opacity: 1.0
+    });
+
+    const baseMap = new VectorTileLayer({
+      source: new VectorTileSource({
+        attributions:
+          '&copy; OpenStreetMap contributors, Who’s On First, ' +
+          'Natural Earth, and osmdata.openstreetmap.de',
+        format: new MVT(),
+        maxZoom: 5,
+        url: '/data/world/{z}/{x}/{y}.pbf',
+      }),
+      style: (feature) => {
+        const layerName = feature.get('layer');
+        return layerStyles[layerName] ?? defaultStyle;
+      },
+    });
+
     const initialMap = new Map({
       target: 'mapDiv',
-      layers: [
-        new VectorTileLayer({
-          source: new VectorTileSource({
-            attributions:
-              '&copy; OpenStreetMap contributors, Who’s On First, ' +
-              'Natural Earth, and osmdata.openstreetmap.de',
-            format: new MVT(),
-            maxZoom: 5,
-            url: '/data/world/{z}/{x}/{y}.pbf',
-          }),
-          style: (feature) => {
-            const layerName = feature.get('layer');
-            return layerStyles[layerName] ?? defaultStyle;
-          },
-        })
-        ,
-        new TileLayer({
-          source: new XYZ({
-            url: '/data/an_gambiae/{z}/{x}/{y}.png',
-            maxZoom: 5,
-          }),
-          opacity: 1.0
-        })
-      ],
+      layers: [ baseMap , an_gambiae ],
       view: new View({
         center: transform([20, -5], 'EPSG:4326', 'EPSG:3857'),
         zoom: 4
       })
     });
 
+    const opacityInput:any = document.getElementById('opacity-input');
+    const opacityOutput:any = document.getElementById('opacity-output');
+    function update() {
+      const opacity = parseFloat(opacityInput.value);
+      an_gambiae.setOpacity(opacity);
+      opacityOutput.innerText = opacity.toFixed(2);
+    }
+    opacityInput.addEventListener('input', update);
+    update();
+
     return () => initialMap.setTarget(undefined);
   }, [layerStyles]);
+
   return (
-    <div id='mapDiv' ref={mapElement} style={{height:'90vh', width: '99.3vw'}}></div>
+    <>
+      <div id='mapDiv' ref={mapElement} style={{height:'90vh', width: '99.3vw'}}></div>
+      <label>
+        Layer opacity
+        <input id="opacity-input" type="range" min="0" max="1" step="0.01" value="1" />
+        <span id="opacity-output"></span>
+      </label>
+    </>
   );
 
 };
