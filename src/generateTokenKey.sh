42d8b42270dfa6c079275b78eaa41f75f0a27c44
kita <andrew.kitchener@tessella.com>
2022-09-13 16:42:20 +0100

    Added token generation script, with SMG comments

---
+++ src/generateTokenKey.sh
@@ -0,0 +1,5 @@
+key=`head -c 20 /dev/random | base64`
+echo "${key}"
+sed -i "s/<token-key>/$key/g" UI/.env.production;
+sed -i "s/<token-key>/$key/g" UI/.env.local;
+sed -i "s/<token-key>/$key/g" API/.envrc;
