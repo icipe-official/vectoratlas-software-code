c9c7dcdb6b1fc154e24891cad76db84def34c8ad
Ray Millward <raymond.millward@capgemini.com>
2022-09-25 21:32:47 +0100

    Linting fixes

--- src/UI/components/home/aboutBanner.tsx
+++ src/UI/components/home/aboutBanner.tsx
@@ -8,26 +8,26 @@ export default function AboutBanner() {
         justifyContent: 'justify-between',
         alignItems: 'center',
       }}>
-        <Grid container>
-          <Grid item lg={4} md={6} sm={12}>
-            <Typography variant='h4' color='primary' py='25px' px='25px'>
+      <Grid container>
+        <Grid item lg={4} md={6} sm={12}>
+          <Typography variant='h4' color='primary' py='25px' px='25px'>
               Analyses-Ready Data and Spatial Models Specifically Tailored to Inform Malaria Vector Control
-            </Typography>
-          </Grid>
-          <Grid item lg={8} md={6} sm={12}>
-            <Box px='25px' py='25px'>
-              <Typography mb='25px' variant='body1' textAlign='justify'>
+          </Typography>
+        </Grid>
+        <Grid item lg={8} md={6} sm={12}>
+          <Box px='25px' py='25px'>
+            <Typography mb='25px' variant='body1' textAlign='justify'>
                 The Vector Atlas brings together a new collaboration of partners (icipe, University of Oxford, MAP, PAMCA, GBIF, VectorBase, IRMapper, BMGF) in an initiative to build an online, open access
                 repository to hold and share our analyses-ready malaria vector occurrence, bionomics, abundance, and insecticide resistance data. Our data will be fully up to date and form the basis of a
                 series of spatial models specifically tailored to inform the control of mosquito vectors of disease.
-              </Typography>
-              <Grid container justifyContent='flex-end'>
-                <Button variant='contained'>Join Mailing List</Button>
-                <Button variant='outlined'>Find out more</Button>
-              </Grid>
-            </Box>
-          </Grid>
+            </Typography>
+            <Grid container justifyContent='flex-end'>
+              <Button variant='contained'>Join Mailing List</Button>
+              <Button variant='outlined'>Find out more</Button>
+            </Grid>
+          </Box>
         </Grid>
+      </Grid>
     </Paper>
   );
 }
