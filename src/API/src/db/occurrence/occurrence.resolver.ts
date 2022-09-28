85dc1eec3e0a0b55e9ba4162a3385ddbf97ca32a
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-09-28 11:53:15 +0100

    Forgot to push linting corrections

--- src/API/src/db/occurrence/occurrence.resolver.ts
+++ src/API/src/db/occurrence/occurrence.resolver.ts
@@ -1,14 +1,25 @@
-import { Args, Parent, Query, ResolveField, ResolveProperty, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Parent,
+  Query,
+  ResolveField,
+  ResolveProperty,
+  Resolver,
+} from '@nestjs/graphql';
 import { OccurrenceService } from './occurrence.service';
 import { Occurrence } from './entities/occurrence.entity';
 import { Site } from '../shared/entities/site.entity';
 import { SiteService } from '../shared/site.service';
-import { Sample } from './entities/sample.entity'
-import { SampleService } from './sample.service'
+import { Sample } from './entities/sample.entity';
+import { SampleService } from './sample.service';
 
 @Resolver(() => Occurrence)
 export class OccurrenceResolver {
-  constructor(private occurrenceService: OccurrenceService, private siteService: SiteService, private sampleService: SampleService) {}
+  constructor(
+    private occurrenceService: OccurrenceService,
+    private siteService: SiteService,
+    private sampleService: SampleService,
+  ) {}
 
   @Query(() => Occurrence)
   async geoData(@Args('id', { type: () => String }) id: string) {
@@ -20,13 +31,13 @@ export class OccurrenceResolver {
     return this.occurrenceService.findAll();
   }
 
-  @ResolveField('site', returns => Site)
-  async getSite(@Parent() parent: Occurrence) : Promise<Site> {
-    return await this.siteService.findOneById(parent.site.id)
+  @ResolveField('site', (returns) => Site)
+  async getSite(@Parent() parent: Occurrence): Promise<Site> {
+    return await this.siteService.findOneById(parent.site.id);
   }
 
-  @ResolveField('sample', returns => Sample)
-  async getSample(@Parent() parent: Occurrence) : Promise<Sample> {
-    return await this.sampleService.findOneById(parent.sample.id)
+  @ResolveField('sample', (returns) => Sample)
+  async getSample(@Parent() parent: Occurrence): Promise<Sample> {
+    return await this.sampleService.findOneById(parent.sample.id);
   }
 }
