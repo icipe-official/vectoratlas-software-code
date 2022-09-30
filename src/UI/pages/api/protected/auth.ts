5ea40b716f9098ca616b3828beec4a253f6b6d5f
kita <andrew.kitchener@tessella.com>
2022-09-30 13:19:09 +0100

    Further linting fixes

--- src/UI/pages/api/protected/auth.ts
+++ src/UI/pages/api/protected/auth.ts
@@ -2,13 +2,16 @@ import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 
 export default withApiAuthRequired(async function ProtectedRoute(req, res) {
   const session = getSession(req, res);
-  const tokenResponse = await fetch(process.env.NEXT_PUBLIC_AUTH_ENDPOINT ?? '', {
-    method: 'GET',
-    credentials: 'include',
-    headers: {
-      Authorization: 'Bearer ' + session?.accessToken,
-    },
-  });
+  const tokenResponse = await fetch(
+    process.env.NEXT_PUBLIC_AUTH_ENDPOINT ?? '',
+    {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        Authorization: 'Bearer ' + session?.accessToken,
+      },
+    }
+  );
   const token = await tokenResponse.text();
   res.status(200).json(token);
 });
