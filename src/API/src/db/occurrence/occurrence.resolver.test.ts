413c8e619ead4b703745b706c43b52674428dad6
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-10-07 15:31:35 +0100

    API linting

--- src/API/src/db/occurrence/occurrence.resolver.test.ts
+++ src/API/src/db/occurrence/occurrence.resolver.test.ts
@@ -7,33 +7,33 @@ import { SampleService } from './sample.service';
 describe('OccurrenceResolver', () => {
   let resolver: OccurrenceResolver;
 
-
-
   const occurrenceService = {
-  findOccurrences: jest.fn(() => {
-    return {items:[{name:'mockOcc1'},{name:'mockOcc2'}], total:2}
-  }),
-  findOneById: jest.fn(),
-  findAll: jest.fn(),
-  }
+    findOccurrences: jest.fn(() => {
+      return { items: [{ name: 'mockOcc1' }, { name: 'mockOcc2' }], total: 2 };
+    }),
+    findOneById: jest.fn(),
+    findAll: jest.fn(),
+  };
   const sampleService = jest.fn();
   const siteService = jest.fn();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OccurrenceResolver,{
-        provide: OccurrenceService,
-        useValue: occurrenceService,
-      }, 
-      {
-      provide: SampleService,
-      useValue: sampleService
-      },
-      {
-      provide: SiteService,
-      useValue: siteService
-      }
-    ],
+      providers: [
+        OccurrenceResolver,
+        {
+          provide: OccurrenceService,
+          useValue: occurrenceService,
+        },
+        {
+          provide: SampleService,
+          useValue: sampleService,
+        },
+        {
+          provide: SiteService,
+          useValue: siteService,
+        },
+      ],
     }).compile();
 
     resolver = module.get<OccurrenceResolver>(OccurrenceResolver);
@@ -41,19 +41,18 @@ describe('OccurrenceResolver', () => {
 
   it('geoData function calls on findOneById from occurrence.service', () => {
     const mockId = 'mockGeoId';
-    resolver.geoData(mockId)
+    resolver.geoData(mockId);
     expect(occurrenceService.findOneById).toHaveBeenCalled();
-  })
+  });
 
   it('allGeoData function calls on findAll from occurrence.service', () => {
-    resolver.allGeoData()
+    resolver.allGeoData();
     expect(occurrenceService.findAll).toHaveBeenCalled();
-  })
+  });
 
   it('OccurrenceData function calls on findOccurrences with correct arguments', () => {
-    resolver.OccurrenceData({take:2, skip:2})
+    resolver.OccurrenceData({ take: 2, skip: 2 });
     expect(occurrenceService.findOccurrences).toHaveBeenCalled();
-    expect(occurrenceService.findOccurrences).toHaveBeenCalledWith(2,2);
-  })
+    expect(occurrenceService.findOccurrences).toHaveBeenCalledWith(2, 2);
+  });
 });
-
