9f49b626d53793cf330d5307fcab03ce86340ce6
Colin Turner <colin.turner@tessella.com>
2022-10-10 17:29:15 +0100

    Merge http and local tests into one test

--- e2e/cypress/e2e/http-ui-tests.cy.js
+++ e2e/cypress/e2e/http-ui-tests.cy.js
@@ -1,28 +1,18 @@
 import { accountDetails, pageHeadings, URL } from '../support/constants.js';
 
 describe('Web environment test', () => {
-    it('Opens VA homepage from web URL', () => {
+    it('Opens VA homepage from web URL and checks that links to pages exists', () => {
       cy.visit(URL.webURL)
-    }),
-    it('Checks that a banner stating "Alpha Version" can be seen', () => {
       cy.contains('alpha version of the Vector Atlas')
-    }),
-    it('Checks for a "Map" link', () => {
       cy.contains('Map').exists
-    }),
-    it('Checks for an "About" link', () => {
       cy.contains('About').exists
-    }),
-    it('Checks for a "Login" link', () => {
       cy.contains('Login').exists
     })
   })
 
   describe('Vector Atlas help site - web', () => {
-    it('Checks the help site can be accessed - web', () => {
+    it('Checks the help site can be accessed', () => {
       cy.visit(URL.webURL + URL.helpURL)
-    }),
-    it('Checks the heading of the help site', () => {
       cy.get("H1").should("exist").contains(pageHeadings.helpTitle)
     })
   })
--- e2e/cypress/e2e/local-ui-tests.cy.js
+++ e2e/cypress/e2e/local-ui-tests.cy.js
@@ -1,28 +1,18 @@
 import { accountDetails, pageHeadings, URL } from '../support/constants.js';
 
 describe('Local environment test', () => {
-    it('Opens VA homepage in the local environment', () => {
+    it('Opens VA homepage from local URL and checks that links to pages exists', () => {
       cy.visit(URL.localURL)
-    }),
-    it('Checks that a banner stating "Alpha Version" can be seen', () => {
       cy.contains('alpha version of the Vector Atlas')
-    }),
-    it('Checks for a "Map" link', () => {
       cy.contains('Map').exists
-    }),
-    it('Checks for an "About" link', () => {
       cy.contains('About').exists
-    }),
-    it('Checks for a "Login" link', () => {
       cy.contains('Login').exists
     })
   })
 
   describe('Vector Atlas help site - local', () => {
-    it('Checks the help site can be accessed - local', () => {
+    it('Checks the help site can be accessed', () => {
       cy.visit(URL.localURL + URL.helpURL)
-    }),
-    it('Checks the heading of the help site', () => {
       cy.get("H1").should("exist").contains(pageHeadings.helpTitle)
     })
   })
