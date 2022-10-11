9214f7f64710f44c17d0a9c35fef270921985b1c
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-10-11 13:34:55 +0100

    csv file can now be accessed via api
    localhost:3001/export/download-all

--- src/API/src/export/export.controller.ts
+++ src/API/src/export/export.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Get, StreamableFile } from '@nestjs/common';
+import { Controller, Get, Res, StreamableFile } from '@nestjs/common';
 import { createReadStream } from 'fs';
 
 @Controller('export')
 export class ExportController {
-  @Get('stream-all-file')
-  getAllOccurrenceData(): StreamableFile {
-    const file = createReadStream(
-      `${process.cwd()}/public/downloads/downloadAllFile.csv`,
-    );
-    return new StreamableFile(file);
+  @Get('download-all')
+  getAllOccurrenceData(@Res() res): StreamableFile {
+    const fileName = 'downloadAllFile.csv';
+    return res.download(`${process.cwd()}/public/downloads/` + fileName);
   }
 }
