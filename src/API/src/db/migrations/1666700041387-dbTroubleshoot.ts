9878578fb12c5e58c796f0d76ea108222f469562
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-10-28 09:55:29 +0100

    Returning migrations to previous state (identical to main)

--- src/API/src/db/migrations/1666700041387-dbTroubleshoot.ts
+++ src/API/src/db/migrations/1666700041387-dbTroubleshoot.ts
@@ -1,26 +1,30 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class dbTroubleshoot1666700041387 implements MigrationInterface {
-    name = 'dbTroubleshoot1666700041387'
+export class itnAndNotesEnvCorrections1666602427108 implements MigrationInterface {
+    name = 'itnAndNotesEnvCorrections1666602427108'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "environment" ("id" character varying(256) NOT NULL, "roof" character varying(50), "walls" character varying(50), "house_screening" boolean, "open_eaves" boolean, "cooking" character varying(50), "housing_notes" character varying, "occupation" character varying(50), "outdoor_activities_night" boolean, "sleeping_outdoors" boolean, "community_notes" character varying, "farming" character varying(50), "farming_notes" character varying, "livestock" character varying(50), "livestock_notes" character varying, "local_plants" character varying(50), CONSTRAINT "PK_f0ec97d0ac5e0e2f50f7475699f" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "recorded_species" ADD "species_notes" character varying`);
-        await queryRunner.query(`ALTER TABLE "bionomics" ADD "study_sampling_design" character varying(50)`);
-        await queryRunner.query(`ALTER TABLE "bionomics" ADD "itn_use" boolean`);
-        await queryRunner.query(`ALTER TABLE "bionomics" ADD "environmentId" character varying(256)`);
-        await queryRunner.query(`ALTER TABLE "bionomics" ADD CONSTRAINT "UQ_b7b6e0077e7331785abad7f2b1e" UNIQUE ("environmentId")`);
-        await queryRunner.query(`ALTER TABLE "bionomics" ADD CONSTRAINT "FK_b7b6e0077e7331785abad7f2b1e" FOREIGN KEY ("environmentId") REFERENCES "environment"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "bionomics" RENAME COLUMN "ITN_use" TO "itn_use"`);
+        await queryRunner.query(`ALTER TABLE "environment" DROP COLUMN "housing_notes"`);
+        await queryRunner.query(`ALTER TABLE "environment" ADD "housing_notes" character varying`);
+        await queryRunner.query(`ALTER TABLE "environment" DROP COLUMN "community_notes"`);
+        await queryRunner.query(`ALTER TABLE "environment" ADD "community_notes" character varying`);
+        await queryRunner.query(`ALTER TABLE "environment" DROP COLUMN "farming_notes"`);
+        await queryRunner.query(`ALTER TABLE "environment" ADD "farming_notes" character varying`);
+        await queryRunner.query(`ALTER TABLE "environment" DROP COLUMN "livestock_notes"`);
+        await queryRunner.query(`ALTER TABLE "environment" ADD "livestock_notes" character varying`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "bionomics" DROP CONSTRAINT "FK_b7b6e0077e7331785abad7f2b1e"`);
-        await queryRunner.query(`ALTER TABLE "bionomics" DROP CONSTRAINT "UQ_b7b6e0077e7331785abad7f2b1e"`);
-        await queryRunner.query(`ALTER TABLE "bionomics" DROP COLUMN "environmentId"`);
-        await queryRunner.query(`ALTER TABLE "bionomics" DROP COLUMN "itn_use"`);
-        await queryRunner.query(`ALTER TABLE "bionomics" DROP COLUMN "study_sampling_design"`);
-        await queryRunner.query(`ALTER TABLE "recorded_species" DROP COLUMN "species_notes"`);
-        await queryRunner.query(`DROP TABLE "environment"`);
+        await queryRunner.query(`ALTER TABLE "environment" DROP COLUMN "livestock_notes"`);
+        await queryRunner.query(`ALTER TABLE "environment" ADD "livestock_notes" character varying(50)`);
+        await queryRunner.query(`ALTER TABLE "environment" DROP COLUMN "farming_notes"`);
+        await queryRunner.query(`ALTER TABLE "environment" ADD "farming_notes" character varying(50)`);
+        await queryRunner.query(`ALTER TABLE "environment" DROP COLUMN "community_notes"`);
+        await queryRunner.query(`ALTER TABLE "environment" ADD "community_notes" character varying(50)`);
+        await queryRunner.query(`ALTER TABLE "environment" DROP COLUMN "housing_notes"`);
+        await queryRunner.query(`ALTER TABLE "environment" ADD "housing_notes" character varying(50)`);
+        await queryRunner.query(`ALTER TABLE "bionomics" RENAME COLUMN "itn_use" TO "ITN_use"`);
     }
 
 }
