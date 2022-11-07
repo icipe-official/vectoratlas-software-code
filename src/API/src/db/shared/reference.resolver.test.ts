15362ee37350b4cecf8ce7f027ae440621bbb794
kita <andrew.kitchener@tessella.com>
2022-11-07 12:58:43 +0000

    Lint fixes

--- src/API/src/db/shared/reference.resolver.test.ts
+++ src/API/src/db/shared/reference.resolver.test.ts
@@ -1,27 +1,27 @@
-import { ReferenceResolver } from "./reference.resolver";
+import { ReferenceResolver } from './reference.resolver';
 
 describe('Reference resolver', () => {
-    let resolver: ReferenceResolver;
-    let referenceService;
+  let resolver: ReferenceResolver;
+  let referenceService;
 
-    beforeEach(() => {
-        referenceService = {
-          findOneById: jest.fn(),
-          findAll: jest.fn(),
-        };
-    
-        resolver = new ReferenceResolver(referenceService);
-      });
+  beforeEach(() => {
+    referenceService = {
+      findOneById: jest.fn(),
+      findAll: jest.fn(),
+    };
 
-      it('referenceData delegates finding one by id to reference service', () => {
-        resolver.referenceData('123');
-    
-        expect(referenceService.findOneById).toHaveBeenCalledWith('123');
-      });
-    
-      it('allReferenceData delegates finding all to reference service', () => {
-        resolver.allReferenceData();
-    
-        expect(referenceService.findAll).toHaveBeenCalled();
-      });
+    resolver = new ReferenceResolver(referenceService);
+  });
+
+  it('referenceData delegates finding one by id to reference service', () => {
+    resolver.referenceData('123');
+
+    expect(referenceService.findOneById).toHaveBeenCalledWith('123');
+  });
+
+  it('allReferenceData delegates finding all to reference service', () => {
+    resolver.allReferenceData();
+
+    expect(referenceService.findAll).toHaveBeenCalled();
+  });
 });
