781bba656a8977cf86d370837d9b3d586e57661a
Ray Millward <raymond.millward@capgemini.com>
2022-11-11 12:28:09 +0000

    Added tests for the filter dropdown

---
+++ src/UI/components/map/layers/filters/filterDropDown.test.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { filterHandler, initialState } from "../../../../state/map/mapSlice";
+import { fireEvent, render, within } from "../../../../test_config/render";
+import FilterDropDown from './filterDropDown';
+
+jest.mock('@mui/material/Autocomplete', () => (props) => (
+  <div>
+    <div>Autocomplete mock {JSON.stringify(props)}</div>
+    <input role="input" onChange={(e) => props.onChange(e, e.target.value)} />
+  </div>
+))
+
+describe("FilterDropDown", () => {
+  let state;
+
+  beforeEach(() => {
+    state = { map: initialState() };
+  })
+
+  it('renders correctly for a given filter', () => {
+    state.map.filterValues.country = ["Kenya", "Uganda", "Tanzania"]
+
+    const { wrapper } = render(
+      <FilterDropDown filterTitle={'Country'} filterName="country" />,
+      state
+    );
+
+    expect(wrapper.container).toMatchSnapshot();
+  })
+
+  it('dispatches the correct filter update when the value changes', async () => {
+    const { wrapper, store } = render(
+      <FilterDropDown filterTitle={'Country'} filterName="country" />,
+      state
+    );
+
+    const input = await wrapper.getByRole('input')
+    fireEvent.change(input, {target: {value: "Kenya" }})
+
+    expect(store.getActions()[0]).toEqual(filterHandler({
+      filterName: 'country',
+      filterOptions: "Kenya"
+    }))
+  })
+})
