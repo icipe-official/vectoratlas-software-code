c9979e7fe715b98e97b96cd4a6dc264de27f88a1
kita <andrew.kitchener@tessella.com>
2022-11-15 10:30:41 +0000

    Linting

--- src/UI/pages/api/auth/[...auth0].ts
+++ src/UI/pages/api/auth/[...auth0].ts
@@ -1,13 +1,16 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-const getLoginState: any = (req: { headers: { referer: any; }; }, loginOptions: any) => {
+const getLoginState: any = (
+  req: { headers: { referer: any } },
+  loginOptions: any
+) => {
   return {
-      returnTo: req.headers.referer,
-      authorizationParams: {
-        audience: process.env.AUTH0_AUDIENCE,
-        // Add the `offline_access` scope to also get a Refresh Token
-        scope: 'openid profile email read',
-      },
+    returnTo: req.headers.referer,
+    authorizationParams: {
+      audience: process.env.AUTH0_AUDIENCE,
+      // Add the `offline_access` scope to also get a Refresh Token
+      scope: 'openid profile email read',
+    },
   };
 };
 
@@ -15,7 +18,7 @@ export default handleAuth({
   async login(req, res) {
     try {
       await handleLogin(req, res, {
-        getLoginState
+        getLoginState,
       });
     } catch (error: any) {
       res.status(error.status || 400).end(error.message);
