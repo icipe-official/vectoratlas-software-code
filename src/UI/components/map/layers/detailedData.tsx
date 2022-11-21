8d6ea718b3ad2bb248ab0e538349e31a1857a29f
kita <andrew.kitchener@tessella.com>
2022-11-21 12:15:48 +0000

    Finished detailed data view

--- src/UI/components/map/layers/detailedData.tsx
+++ src/UI/components/map/layers/detailedData.tsx
@@ -1,6 +1,14 @@
-import { Box, Grid, Typography } from '@mui/material';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Grid,
+  Typography,
+} from '@mui/material';
 import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function DetailedData({ data }) {
   const season = data.bionomics?.season_given
@@ -58,16 +66,79 @@ export default function DetailedData({ data }) {
               SAMPLING METHOD:{' '}
             </Typography>
             <Typography display="inline">
-              {' '}
               {data.sample.mossamp_tech_1}
             </Typography>
           </Grid>
           <Grid item>
             {season === 'rainy' ? (
-              <ThunderstormIcon />
+              <ThunderstormIcon sx={{ fontSize: '1.3rem' }} />
             ) : season === 'dry' ? (
-              <WbSunnyIcon />
+              <WbSunnyIcon sx={{ fontSize: '1.3rem' }} />
             ) : null}
+          </Grid>
+        </Grid>
+        <Grid container direction="row" justifyContent="space-between">
+          <Grid item xs={12}>
+            <Accordion
+              disableGutters
+              sx={{ margin: 0, padding: 0, boxShadow: 'none' }}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                sx={{
+                  margin: 0,
+                  padding: 0,
+                  minHeight: 0,
+                  '& .MuiAccordionSummary-content': {
+                    margin: 0,
+                  },
+                }}
+              >
+                <Typography
+                  display="inline"
+                  variant="inherit"
+                  color="primary"
+                  fontSize={12}
+                >
+                  SOURCE
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Grid container direction="row" justifyContent="space-between">
+                  <Grid item>
+                    <Typography
+                      display="inline"
+                      variant="inherit"
+                      color="primary"
+                      fontSize={12}
+                    >
+                      AUTHOR:
+                    </Typography>
+                    <Typography display="inline">
+                      {' '}
+                      {data.reference.author}
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography
+                      display="inline"
+                      variant="inherit"
+                      color="primary"
+                      fontSize={12}
+                    >
+                      YEAR:
+                    </Typography>
+                    <Typography display="inline">
+                      {' '}
+                      {data.reference.year}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <Typography>{data.reference.citation}</Typography>
+                </Grid>
+              </AccordionDetails>
+            </Accordion>
           </Grid>
         </Grid>
       </Grid>
