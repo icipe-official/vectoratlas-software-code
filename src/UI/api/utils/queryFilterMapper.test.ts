bb61376fe82dd9dc4b90131cb5e3cdb0ebdd9dc1
Ray Millward <raymond.millward@capgemini.com>
2022-11-22 15:48:07 +0000

    Updated the UI to use arrays for country and species filters

--- src/UI/api/utils/queryFilterMapper.test.ts
+++ src/UI/api/utils/queryFilterMapper.test.ts
@@ -1,6 +1,6 @@
 import { queryFilterMapper } from './queryFilterMapper';
 
-describe(queryFilterMapper.name, () => {
+describe("queryFilterMapper", () => {
   it('maps a filter object as expected', () => {
     const testFilter: any = {
       extractValueString: { value: 'testString' },
@@ -26,4 +26,15 @@ describe(queryFilterMapper.name, () => {
     };
     expect(queryFilterMapper(testFilter)).toEqual(expectTestReturn);
   });
+
+  it('ignores properties with empty lists', () => {
+    const testFilter = {
+      nonEmptyList: { value: ['item1', 'item2']},
+      emptyList: { value: [] }
+    }
+
+    expect(queryFilterMapper(testFilter)).toEqual({
+      nonEmptyList: ['item1', 'item2']
+    })
+  })
 });
