49b4c3692988446aa0a6802d3ee0dc3667c65ab3
shishi5089 <shishinangs83@gmail.com>
2022-11-23 14:08:54 +0300

    Map download button updated changes

--- src/UI/components/map/layers/filters/dateFilter.tsx
+++ src/UI/components/map/layers/filters/dateFilter.tsx
@@ -9,6 +9,21 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import { TimeRange } from '../../../../state/state.types';
 
+const getTimezoneOffset = (value: Date) => value.getTimezoneOffset() * 60000;
+const makeLocalAppearUTC = (value: number | null) => {
+  if (!value) {
+    return value
+  }
+  const dateTime = new Date(value);
+  const utcFromLocal = new Date(dateTime.getTime() + getTimezoneOffset(dateTime));
+  return utcFromLocal;
+};
+
+const localToUTC = (dateTime: Date) => {
+  const utcFromLocal = new Date(dateTime.getTime() - getTimezoneOffset(dateTime));
+  return utcFromLocal;
+};
+
 export default function DateFilter(props: any) {
   const dispatch = useAppDispatch();
 
@@ -18,12 +33,13 @@ export default function DateFilter(props: any) {
   const valueTo = timeRange ? timeRange.end : null;
 
   const handleChange = (event: any, timeSelect: string) => {
+    console.log(localToUTC(event))
     if (timeSelect === 'from') {
       dispatch(
         filterHandler({
           filterName: props.filterName,
           filterOptions: {
-            start: event ? event.getTime() : null,
+            start: event ? localToUTC(event).getTime() : null,
             end: valueTo,
           },
         })
@@ -34,7 +50,7 @@ export default function DateFilter(props: any) {
           filterName: props.filterName,
           filterOptions: {
             start: valueFrom,
-            end: event ? event.getTime() : null,
+            end: event ? localToUTC(event).getTime() : null,
           },
         })
       );
@@ -64,7 +80,7 @@ export default function DateFilter(props: any) {
                 label="From"
                 minDate={new Date('1980-01-01')}
                 maxDate={new Date()}
-                value={valueFrom}
+                value={makeLocalAppearUTC(valueFrom)}
                 onChange={(e) => handleChange(e, 'from')}
                 renderInput={(params) => (
                   <TextField size="small" {...params} helperText={null} />
@@ -79,7 +95,7 @@ export default function DateFilter(props: any) {
                 label="To"
                 minDate={valueFrom ?? new Date('1980-01-01')}
                 maxDate={new Date()}
-                value={valueTo}
+                value={makeLocalAppearUTC(valueTo)}
                 onChange={(e) => handleChange(e, 'to')}
                 renderInput={(params) => (
                   <TextField size="small" {...params} helperText={null} />
