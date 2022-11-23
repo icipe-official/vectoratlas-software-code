430a87741d24edff008b6fd273c83b4b28620cf0
shishi5089 <shishinangs83@gmail.com>
2022-11-23 10:25:51 +0300

    Map download button updated changes

--- src/UI/components/map/layers/filters/downloadMap.test.tsx
+++ src/UI/components/map/layers/filters/downloadMap.test.tsx
@@ -1,1 +1,10 @@
 import React from 'react';
+import DownloadMap from './downloadMap';
+import { render } from '@testing-library/react';
+
+describe('DownloadMap component', () => {
+    it('renders', () => {
+      render(<DownloadMap />);
+    });
+  });
+  
