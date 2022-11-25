d993594dae6d5f30feb90a5b28a134fc2a3c14b8
kita <andrew.kitchener@tessella.com>
2022-11-25 09:32:58 +0000

    Fixed UI behaviour for logged-in user

--- src/UI/state/authSlice.ts
+++ src/UI/state/authSlice.ts
@@ -34,6 +34,10 @@ export const authSlice = createSlice({
     builder.addCase(getUserInfo.pending, (state) => {
       state.isLoading = true;
     });
+    builder.addCase(getUserInfo.rejected, (state) => {
+      console.log('here')
+      state.isLoading = false;
+    });
     builder.addCase(getUserInfo.fulfilled, (state, action: any) => {
       state.roles = action.payload.roles;
       state.token = action.payload.token;
