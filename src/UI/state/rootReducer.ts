3e306fdd6adf4e510c9c53d423c88ed77d1f0989
Ray Millward <raymond.millward@capgemini.com>
2022-11-29 08:58:16 +0000

    Initial updates for editing species information

--- src/UI/state/rootReducer.ts
+++ src/UI/state/rootReducer.ts
@@ -3,12 +3,14 @@ import configReducer from './configSlice';
 import mapReducer from './map/mapSlice';
 import authReducer from './authSlice';
 import sourceReducer from './sourceSlice';
+import speciesInfoReducer from './speciesInformation/speciesInformationSlice';
 
 const rootReducer = combineReducers({
   config: configReducer,
   map: mapReducer,
   auth: authReducer,
   source: sourceReducer,
+  speciesInfo: speciesInfoReducer,
 });
 
 export default rootReducer;
--- src/UI/state/state.types.ts
+++ src/UI/state/state.types.ts
@@ -2,9 +2,11 @@ export type TimeRange = {
   start: number | null;
   end: number | null;
 };
+
 export type MapFilter<T> = {
   value: T;
 };
+
 export type VectorAtlasFilters = {
   country: MapFilter<string[] | string>;
   species: MapFilter<string[] | string>;
@@ -19,4 +21,11 @@ export type VectorAtlasFilters = {
     | MapFilter<boolean[]>
     | MapFilter<string[]>
     | MapFilter<TimeRange>;
+};
+
+export type SpeciesInformation = {
+  id: string;
+  name: string;
+  shortDescription: string;
+  description: string;
 };
