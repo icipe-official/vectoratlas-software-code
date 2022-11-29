04c9222aaa423dd274a11f5ead5a5ca7fd5d5477
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-11-29 11:37:42 +0000

    Test updated given new regex

--- src/UI/api/utils/sourceStringValidation.test.ts
+++ src/UI/api/utils/sourceStringValidation.test.ts
@@ -15,12 +15,12 @@ describe('sourceStringValidation', () => {
 
   const testSourceStringObjectExpected: any = {
     test1: 'This is a test string',
-    test2: 'Thi\\\\s i\\\\s a t\\\\est str\\\\ing',
-    test3: '\\\\This is\\\\ a \\\\test string\\\\',
+    test2: 'Thi\\s i\\s a t\\est str\\ing',
+    test3: '\\This is\\ a \\test string',
     // eslint-disable-next-line prettier/prettier
-    test4: 'Th\\\"i\\\"s is a t\\\"e\\\"st s\\\"trin\\\"g',
+    test4: 'Th\"i\"s is a t\"e\"st s\"trin\"g',
     // eslint-disable-next-line prettier/prettier
-    test5: '\\\"This\\\" is \\\"a\\\" test \\\"string\\\"',
+    test5: '\"This\" is \"a\" test \"string\"',
   };
 
   it('call on escapeRegex when required to process string', () => {
