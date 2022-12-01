0b145807fe0b4fcd74a758f1a80457cd30b546e3
Christopher Sandbach <chris.sandbach@capgemini.com>
2022-12-01 17:09:22 +0000

    build fix

--- src/UI/pages/sources.tsx
+++ src/UI/pages/sources.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 import { Container } from '@mui/material';
-import { getSourceInfo } from '../state/source/sourceSlice';
 import { AppDispatch } from '../state/store';
 import { useDispatch } from 'react-redux';
 import SectionPanel from '../components/layout/sectionPanel';
 import dynamic from 'next/dynamic';
+import { getSourceInfo } from '../state/source/actions/getSourceInfo';
 
 const SourceTableNoSsr = dynamic(
   () => import('../components/sources/source_table'),
