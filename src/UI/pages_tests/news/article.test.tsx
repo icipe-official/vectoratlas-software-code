32c3ca819433d2a95e560cb8fe9e6af7d31f3f24
Ray Millward <raymond.millward@capgemini.com>
2022-12-07 21:19:29 +0000

    Added new top level pages for news

---
+++ src/UI/pages_tests/news/article.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NewsArticlePage from '../../pages/news/article';
+import { render } from '../../test_config/render';
+
+jest.mock('../../components/news/newsDetails', () => function NewsDetailsMock() {
+  return (<div>News details mock</div>)
+})
+
+describe('news details page', () => {
+  it('renders correctly', () => {
+    const { wrapper } = render(<NewsArticlePage />);
+
+    expect(wrapper.container).toMatchSnapshot();
+  });
+});
