ab1aaf15a351beb4505354a0f49adf0e0cb616ef
shishi5089 <shishinangs83@gmail.com>
2022-12-19 09:56:11 +0300

    Secured API route

--- src/API/src/ingest/ingest.controller.ts
+++ src/API/src/ingest/ingest.controller.ts
@@ -2,15 +2,22 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Role } from 'src/auth/user_role/role.enum';
+import { Roles } from 'src/auth/user_role/roles.decorator';
+import { RolesGuard } from 'src/auth/user_role/roles.guard';
 import { IngestService } from './ingest.service';
 
 @Controller('ingest')
 export class IngestController {
   constructor(private ingestService: IngestService) {}
 
+  @UseGuards(AuthGuard('va'), RolesGuard)
+  @Roles(Role.Uploader)
   @Post('uploadBionomics')
   @UseInterceptors(FileInterceptor('file'))
   async uploadBionomicsCsv(@UploadedFile() bionomicsCsv: Express.Multer.File) {
@@ -19,6 +26,8 @@ export class IngestController {
     );
   }
 
+  @UseGuards(AuthGuard('va'), RolesGuard)
+  @Roles(Role.Uploader)
   @Post('uploadOccurrence')
   @UseInterceptors(FileInterceptor('file'))
   async uploadOccurrenceCsv(
