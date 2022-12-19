d7bfa0b5c664fb00f9fe0c2e230b166286a6e1da
adess <vansnoden>
2022-12-19 12:47:00 +0300

    double quote change back to single quote

--- src/UI/pages/user_settings.tsx
+++ src/UI/pages/user_settings.tsx
@@ -1,16 +1,16 @@
-import { Avatar, Container } from "@mui/material";
-import React from "react";
-import SectionPanel from "../components/layout/sectionPanel";
-import { useUser } from "@auth0/nextjs-auth0";
-import { useAppSelector } from "../state/hooks";
-import LockOpenIcon from "@mui/icons-material/LockOpen";
-import { Grid, TextField } from "@mui/material";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { Avatar, Container } from '@mui/material';
+import React from 'react';
+import SectionPanel from '../components/layout/sectionPanel';
+import { useUser } from '@auth0/nextjs-auth0';
+import { useAppSelector } from '../state/hooks';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import { Grid, TextField } from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function UserSettings() {
   const { user, isLoading } = useUser();
@@ -18,11 +18,11 @@ function UserSettings() {
   const isLoadingRoles = useAppSelector((state) => state.auth.isLoading);
 
   const router = useRouter();
-  const backHome = () => router.push("./");
+  const backHome = () => router.push('./');
 
   useEffect(() => {
     if (!user?.nickname && !isLoading) {
-      router.push("/api/auth/login");
+      router.push('/api/auth/login');
     }
   }, [user, isLoading, router]);
 
@@ -32,8 +32,8 @@ function UserSettings() {
         <main>
           <Container
             sx={{
-              padding: "10px",
-              maxWidth: "75%",
+              padding: '10px',
+              maxWidth: '75%',
             }}
           >
             <SectionPanel title="User's Settings">
@@ -51,7 +51,7 @@ function UserSettings() {
                       </div>
                     </Grid>
                     {/* <Grid item xs={12} md={6}>
-                                        <Box sx={{ "& > :not(style)": { m: 1 } }}>
+                                        <Box sx={{ '& > :not(style)': { m: 1 } }}>
                                             <Fab color="primary" aria-label="edit">
                                                 <EditIcon />
                                             </Fab>
@@ -91,9 +91,9 @@ function UserSettings() {
                     <div>
                       <List
                         sx={{
-                          width: "100%",
+                          width: '100%',
                           maxWidth: 360,
-                          bgcolor: "background.paper",
+                          bgcolor: 'background.paper',
                         }}
                       >
                         {userRoles.map((role) => {
