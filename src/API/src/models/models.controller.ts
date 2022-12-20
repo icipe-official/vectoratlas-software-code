011718f36e12388fe30d2e08506dfc415db1f823
andykitch <andrew.kitchener@capgemini.com>
2022-12-20 14:16:46 +0000

    Removed unused code

--- src/API/src/models/models.controller.ts
+++ src/API/src/models/models.controller.ts
@@ -3,37 +3,15 @@ import {
   Post,
   UseInterceptors,
   HttpException,
-  HttpStatus,
   UseGuards,
   UploadedFile,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { extname } from 'path';
 import { Role } from 'src/auth/user_role/role.enum';
 import { Roles } from 'src/auth/user_role/roles.decorator';
 import { RolesGuard } from 'src/auth/user_role/roles.guard';
 import { ModelsService } from './models.service';
-
-const multerOptions = {
-  fileFilter: (req: any, file: any, cb: any) => {
-    if (extname(file.originalname).match('^.*.(tif|shp)$')) {
-      // Allow storage of file
-      cb(null, true);
-    } else {
-      // Reject file
-      cb(
-        new HttpException(
-          `Unsupported file type ${extname(
-            file.originalname,
-          )}. Expected .tif or .shp.`,
-          HttpStatus.BAD_REQUEST,
-        ),
-        false,
-      );
-    }
-  },
-};
 
 @Controller('models')
 export class ModelsController {
