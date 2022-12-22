716f48569865591d75e06480d8ccaeba36770f4d
shishi5089 <shishinangs83@gmail.com>
2022-12-22 11:53:45 +0300

    Fixed source form validation

--- src/UI/components/sources/source_form.tsx
+++ src/UI/components/sources/source_form.tsx
@@ -44,6 +44,9 @@ export default function SourceForm() {
     },
   });
   const [year, setYear] = useState<Date | null>(null);
+  const onKeyDown = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+ };
 
   const dispatch = useDispatch<AppDispatch>();
   const onSubmit = async (data: NewSource) => {
@@ -175,9 +178,11 @@ export default function SourceForm() {
                     onChange={(event) => {
                       onChange(event);
                       setYear(event);
+
                     }}
                     renderInput={(params) => (
                       <TextField
+                        onKeyDown={onKeyDown}
                         size="small"
                         {...params}
                         error={!!error}
