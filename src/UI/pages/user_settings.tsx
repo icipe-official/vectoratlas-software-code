2dc31fe1c395b3cbc33ca5bf7ac0762abb0c07df
adess <vansnoden>
2023-01-06 13:22:00 +0300

    user setting code migrated to a component

--- src/UI/pages/user_settings.tsx
+++ src/UI/pages/user_settings.tsx
@@ -11,6 +11,7 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import UserSettingForm from '../components/settings/UserSettingForm';
 
 function UserSettings() {
   const { user, isLoading } = useUser();
@@ -30,94 +31,12 @@ function UserSettings() {
     return (
       <div>
         <main>
-          <Container
-            sx={{
-              padding: '10px',
-              maxWidth: '75%',
-            }}
-          >
-            <SectionPanel title="User's Settings">
-              {/* <AuthWrapper role="">
-                            <div>
-                                Welcome to vector atlas
-                            </div>
-                        </AuthWrapper> */}
-              <Grid container spacing={3}>
-                <Grid item xs={12}>
-                  <Grid container>
-                    <Grid item xs={12} md={6}>
-                      <div>
-                        Welcome <b>{user?.nickname}</b> !
-                      </div>
-                    </Grid>
-                    {/* <Grid item xs={12} md={6}>
-                                        <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                                            <Fab color="primary" aria-label="edit">
-                                                <EditIcon />
-                                            </Fab>
-                                            <Fab color="error" aria-label="add">
-                                                <DeleteForeverIcon />
-                                            </Fab>
-                                        </Box>
-                                    </Grid> */}
-                  </Grid>
-                </Grid>
-                <Grid item sm={12} md={6}>
-                  <div>
-                    <h4 color="primary">Personal information</h4>
-                    <div style={{ marginTop: 30 }}>
-                      <TextField
-                        id="outlined-basic"
-                        label="Name"
-                        variant="outlined"
-                        value={user?.name}
-                        fullWidth={true}
-                      />
-                    </div>
-                    <div style={{ marginTop: 30 }}>
-                      <TextField
-                        id="outlined-basic"
-                        label="Email"
-                        variant="outlined"
-                        value={user?.email}
-                        fullWidth={true}
-                      />
-                    </div>
-                  </div>
-                </Grid>
-                <Grid item sm={12} md={6}>
-                  <div>
-                    <h4 color="primary">Access information</h4>
-                    <div>
-                      <List
-                        sx={{
-                          width: '100%',
-                          maxWidth: 360,
-                          bgcolor: 'background.paper',
-                        }}
-                      >
-                        {userRoles.map((role, index) => (
-                          <ListItem key={index}>
-                            <ListItemAvatar>
-                              <Avatar>
-                                <LockOpenIcon />
-                              </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary={role} />
-                          </ListItem>
-                        ))}
-                      </List>
-                    </div>
-                  </div>
-                </Grid>
-              </Grid>
-            </SectionPanel>
-          </Container>
+          <UserSettingForm user={user} userRoles={userRoles}/>
         </main>
       </div>
     );
   } else {
-    return <div>Hello World !</div>;
+    return <div>welcome to vector atlas platform</div>;
   }
 }
 
