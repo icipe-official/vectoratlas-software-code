cd874db00ed2213960f99de6fb913bcd3c19fcee
Ray Millward <raymond.millward@capgemini.com>
2023-01-11 08:42:11 +0000

    Removed unnecessary comment

--- src/UI/state/map/actions/downloadModelOutput.ts
+++ src/UI/state/map/actions/downloadModelOutput.ts
@@ -5,7 +5,7 @@ export const downloadModelOutput = (blobName: string, blobLocation: string) =>
   createAsyncThunk('map/downloadModelOutput', async () => {
     const fileBlob = await downloadModelOutputData(blobLocation);
 
-    var link = document.createElement('a'); // once we have the file buffer BLOB from the post request we simply need to send a GET request to retrieve the file data
+    var link = document.createElement('a');
     link.href = URL.createObjectURL(
       new Blob([fileBlob], { type: 'image/tiff' })
     );
