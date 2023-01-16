607f1029bfed6d740c6f6e15926904ecc6225a4a
FranckMight1555 <117161561+FranckMight1555@users.noreply.github.com>
2023-01-16 15:50:07 +0300

    #349 Creation of the page and components for the datahub

---
+++ src/UI/pages/dataHub.tsx
@@ -0,0 +1,40 @@
+import { Avatar, Container } from '@mui/material';
+import React from 'react';
+import SectionPanel from '../components/layout/sectionPanel';
+import { useUser } from '@auth0/nextjs-auth0';
+import { useAppSelector } from '../state/hooks';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import { Grid, TextField } from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import DataHubPanel from '../components/datahub/dataHubPanel';
+
+function DataHub() {
+  const { user, isLoading } = useUser();
+  const userRoles = useAppSelector((state) => state.auth.roles);
+  const isLoadingRoles = useAppSelector((state) => state.auth.isLoading);
+
+  const router = useRouter();
+  const backHome = () => router.push('./');
+
+  useEffect(() => {
+    if (!user) {
+      alert("Hello !");
+      router.push('/api/auth/login');
+    }
+  }, [user, isLoading, router]);
+
+  return (
+    <div>
+      <main>
+        <DataHubPanel user={user} userRoles={userRoles} />
+      </main>
+    </div>
+  );
+}
+
+export default DataHub;
