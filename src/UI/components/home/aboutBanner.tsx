e2c080ec158e3ffd78a39b3c54f7e1e4547362bc
csandbach <chris.sandbach@capgemini.com>
2023-01-19 10:43:21 +0000

    Initial mobile design - not happy but spacing improved

--- src/UI/components/home/aboutBanner.tsx
+++ src/UI/components/home/aboutBanner.tsx
@@ -29,13 +29,14 @@ export default function AboutBanner() {
     >
       <picture style={{width:'100%', height: 'fit-content', marginBottom: -6 }}>
         <img
-          src={'home/MapSmall.png'}
+          src={isMobile ? 'home/MapSmallMob.png': 'home/MapSmall.png'}
           style={{ width: '100%', borderRadius:'5px' }}
           alt="placeholder"
         />
         </picture>
-        <div style={{position: 'absolute', display: 'flex', justifyContent: 'end', right: 20, bottom: '0.5vw', background: 'rgba(256,256,256,0)', padding: 20, borderRadius: '5px' }}>
+        <div style={{position: 'absolute', display: 'flex', justifyContent: 'end', right: 0, bottom: '0.5vw', background: 'rgba(256,256,256,0)', padding: 15, borderRadius: '5px' }}>
           <div style={{display: 'flex', flexDirection:'column', alignItems:'end'}}>
+            {isMobile ? <></> :
             <picture style={{width:'30vw'}}>
               <img
                 src="/vector-atlas-logo.svg"
@@ -43,18 +44,56 @@ export default function AboutBanner() {
                 alt="Vector Atlas logo"
               />
             </picture>
-            <div style={{width:'70%',padding:'5px',paddingRight:0, marginTop: '5px', borderRadius: '5px', marginLeft: 'auto'}}>
-              <Typography variant="body1" sx={{ textAlign:'right', background: 'rgba(150, 180, 190, 0.5)', borderRadius:'5px', paddingX: 1, fontSize:'1.5vw'}}>
-                Analyses-ready data and spatial models tailored for malaria vector control
-              </Typography>
+            }
+            <div style={isMobile ? {width:'100%',padding:'5px',paddingRight:0, marginTop: '5px', borderRadius: '5px'} : 
+              {width:'70%',padding:'5px',paddingRight:0, marginTop: '5px', borderRadius: '5px'}}>
+              {isMobile ? 
+                <div style={{width:'100%'}}>
+                  <div style={{display:'flex', background:'rgb(3, 133, 67)', borderRadius:'5px', marginBottom:3, width: 'fit-content'}}>
+                    <picture style={{display: 'flex', alignItems:'center'}}>
+                      <img
+                      src="mosquito.svg"
+                      style={{ width: 20, paddingTop: '2px',margin:'2px' }}
+                      alt="placeholder"
+                    />
+                    </picture>
+                    <Typography style={{paddingRight:2}}>Malaria Vector Control</Typography>
+                  </div>
+                  <div style={{display:'flex', background:'rgb(3, 133, 67)', borderRadius:'5px', marginBottom:3, width: 'fit-content'}}>
+                    <picture style={{display: 'flex', alignItems:'center'}}>
+                      <img
+                      src="mosquito.svg"
+                      style={{ width: 20, paddingTop: '2px',margin:'2px' }}
+                      alt="placeholder"
+                    />
+                    </picture>
+                    <Typography style={{paddingRight:2}}>Analysis Ready Data</Typography>
+                  </div>
+                  <div style={{display:'flex', background:'rgb(3, 133, 67)', borderRadius:'5px', marginBottom:3, width: 'fit-content'}}>
+                    <picture style={{display: 'flex', alignItems:'center'}}>
+                      <img
+                      src="mosquito.svg"
+                      style={{ width: 20, paddingTop: '2px',margin:'2px' }}
+                      alt="placeholder"
+                    />
+                    </picture>
+                    <Typography style={{paddingRight:2}}>Spatial Models</Typography>
+                  </div>
+
+                </div>:
+                <Typography variant="body1" sx={{ textAlign:'right', background: 'rgba(150, 180, 190, 0.5)', borderRadius:'5px', paddingX: 1, fontSize:'1.5vw'}}>
+                  Analyses-ready data and spatial models tailored for malaria vector control
+                </Typography>
+              } 
             </div>
               <Button variant='contained' sx={{display: 'flex', width:'30vw',alignItems: 'center', background: 'black', padding:'px',marginTop: '5px',marginLeft: 'auto'}}>
-                <Typography noWrap variant="body1" sx={{marginLeft:'10px', fontSize:'1.5vw', overflow: "hidden", textOverflow: "ellipsis"}}>
+                <Typography noWrap variant="body1" sx={isMobile ? { fontSize:'2vw'} : 
+                  {marginLeft:'10px', fontSize:'1.5vw'}}>
                   Explore the Data
                 </Typography>
-                <MapIcon sx={{marginLeft:'10px', marginRight: '10px', fontSize: '2vw'}}/>
+                {isMobile ? <></> :  <MapIcon sx={{marginLeft:'10px', marginRight: '10px', fontSize: '2vw'}}/>}
               </Button>
-              {isMobile ? <div>T</div> : <></>}
+              {isMobile ? <div></div> : <></>}
           </div>
         </div>
     </Paper>
