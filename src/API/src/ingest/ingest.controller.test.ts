21bc5d71ebf7edddf73edb16c5be009fb7ece1bb
shishi5089 <shishinangs83@gmail.com>
2023-01-25 13:15:22 +0300

    Revert "'fit' edited to 'it'"
    This reverts commit a8b8e082c80e3a7b92f4d653a9f9821cb75daa22.

--- src/API/src/ingest/ingest.controller.test.ts
+++ src/API/src/ingest/ingest.controller.test.ts
@@ -139,7 +139,7 @@ describe('IngestController', () => {
       expect(guards[1]).toBe(RolesGuard);
     });
 
-    it('should send email', async () => {
+    fit('should send email', async () => {
       process.env.REVIEWER_EMAIL_LIST = 'test@reviewer.com';
       const user = {
         sub: 'existing',
@@ -252,7 +252,7 @@ describe('IngestController', () => {
       expect(guards[1]).toBe(RolesGuard);
     });
 
-    it('should send email', async () => {
+    fit('should send email', async () => {
       process.env.REVIEWER_EMAIL_LIST = 'test@reviewer.com';
       const user = {
         sub: 'existing',
