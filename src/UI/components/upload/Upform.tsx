fc6ace16c5443a3aaa0391888ef8e929fe3e5518
salvador atagong <atagong5@gmail.com>
2023-01-27 04:28:22 +0300

    message displayal on upload attempt OK, still need to validate the process with the api...

--- src/UI/components/upload/Upform.tsx
+++ src/UI/components/upload/Upform.tsx
@@ -8,13 +8,56 @@ import * as React from 'react';
 function Upform() {
   const [openSuccess, setOpenSuccess] = React.useState(false);
   const [openFailure, setOpenFailure] = React.useState(false);
+  const [formStatus, setFormStatus] = React.useState(false);
+  
+  const [query, setQuery] = React.useState({
+      file: ""
+  })
 
-  const handleClick = () => {
-    if (upload_data()) {
-      setOpenSuccess(true);
-    } else {
-      setOpenFailure(true);
+  const validated_response = (res:any)=>{
+    // res is an array of arrays:
+    let test = false;
+    if(Array.isArray(res)){
+      test = true;
+      for(let i=0; i<res.length; i++){
+        if (res[i].length === 0){
+          test = test && true;
+        }else{
+          test = test && false;
+        }
+      }
     }
+    return test;
+  }
+
+  const upload_data = async (e:any) => {
+    // call endpoint here for real upload process
+    e.preventDefault();
+    const formData = new FormData();
+    Object.entries(query).forEach(([key, value]) => {
+        formData.append(key, value);
+    });
+    console.log(formData);
+    const req = await fetch('/vector-api/ingest/uploadBionomics',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }, 
+      body: JSON.stringify(formData)
+    }).then((res) => {
+      console.log('res');
+      console.log(JSON.stringify(res));
+      if (validated_response(res)) {
+        setOpenSuccess(true);
+      } else {
+        setOpenFailure(true);
+      }
+    })
+    return false;
+  };
+
+  const handleSubmit = (e:any) => {
+    upload_data(e)
   };
 
   const handleClose = (
@@ -27,6 +70,13 @@ function Upform() {
     setOpenSuccess(false);
     setOpenFailure(false);
   };
+
+  const handleFileChange = () => (e:any) => {
+    setQuery((prevState) => ({
+        ...prevState,
+        files: e.target.files[0]
+    }));
+  }
 
   const action = (
     <React.Fragment>
@@ -44,34 +94,31 @@ function Upform() {
     </React.Fragment>
   );
 
-  const upload_data = () => {
-    // call endpoint here for reall upload process
-    return true;
-  };
-
   return (
-    <form>
-      <Box sx={{ height: '75%' }}>
-        <input type="file" name="csv"></input>
-        <Button variant="contained" onClick={handleClick} size="large">
-          Submit
-        </Button>
-        <Snackbar
-          open={openSuccess}
-          autoHideDuration={6000}
-          onClose={handleClose}
-          message="Upload successfull"
-          action={action}
-        />
-        <Snackbar
-          open={openFailure}
-          autoHideDuration={6000}
-          onClose={handleClose}
-          message="Upload Failed"
-          action={action}
-        />
-      </Box>
-    </form>
+    <div>
+      <form method="POST" encType="multipart/form-data"  onSubmit={handleSubmit}>
+        <Box sx={{ height: '75%' }}>
+          <input type="file" name="file" required={true} onChange={handleFileChange()}/>
+          <Button type="submit" variant="contained" size="large">
+            Submit
+          </Button>
+          <Snackbar
+            open={openSuccess}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            message="Upload successfull"
+            action={action}
+          />
+          <Snackbar
+            open={openFailure}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            message="Upload Failed"
+            action={action}
+          />
+        </Box>
+      </form>
+    </div>
   );
 }
 
