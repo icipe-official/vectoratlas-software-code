953b4051610ba0d44650e79976a9875681338b86
vansnoden <atagong5@gmail.com>
2023-01-30 10:10:57 +0300

    dataset review ui layout updated

--- src/UI/components/review/ReviewForm.tsx
+++ src/UI/components/review/ReviewForm.tsx
@@ -1,23 +1,53 @@
-import { Avatar, Button, Grid, List, ListItem, ListItemAvatar, TextField } from "@mui/material";
+import { Avatar, Button, Grid, List, ListItem, ListItemAvatar, TextareaAutosize, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
 
 
 function ReviewForm(props: any){
+    const [dataset, getDataset] = useState(null);
+
+    useEffect(() => {    
+        // get dataset info from db 
+        let dataset_id = props.dataset_id 
+    });
+
     const download_data= () =>{
         alert("Attempt to download dataset identified by: "+props?.dataset_id)
     }
 
     if(props.dataset_id){
         return (
-        <div>
-            <form>
-                <div>
-                    <TextField id="outlined-basic" value={props.dataset_id} label="Dataset ID" variant="outlined" />
-                </div>
-                <div>
-                    <Button variant="contained" onClick={download_data}>Download csv</Button>
-                </div>
-            </form>
-        </div>
+            <div>
+                <Grid container spacing={3}>
+                    <Grid item sm={12} md={4}>
+                        <div>
+                            <TextField id="outlined-basic" value={props.dataset_id} label="Dataset ID" disabled={true} variant="outlined" />
+                        </div>
+                    </Grid>
+                    <Grid item sm={12} md={4}>
+                        <div>
+                            <TextField id="outlined-basic" value={"demo@user.com"} label="Data uploaded by" disabled={true} variant="outlined" />
+                        </div>
+                    </Grid>
+                    <Grid item sm={12} md={4}>
+                        <div>
+                            <Button variant="contained" onClick={download_data}>Download csv</Button>
+                        </div>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={3}>
+                    <Grid item sm={12} md={12}>
+                        <div style={{ marginTop:20 }}>
+                            <label for="area" >Message</label><br/>
+                        </div>
+                        <TextareaAutosize
+                            id="area"
+                            aria-label="Message"
+                            minRows={3}
+                            placeholder="..."
+                            style={{ width: 400, marginTop:20 }}/>
+                    </Grid>
+                </Grid>
+            </div>
         ) 
     }else{
         return (
