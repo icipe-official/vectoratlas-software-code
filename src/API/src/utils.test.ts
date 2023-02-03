7477b2abbfff3d65389934776a811e5a17c6ba03
andykitch <andrew.kitchener@capgemini.com>
2023-02-03 06:36:40 +0000

    Added tests for utils

--- src/API/src/utils.test.ts
+++ src/API/src/utils.test.ts
@@ -1,4 +1,13 @@
-import { isEmpty, makeDate } from './utils';
+import * as fs from 'fs';
+import { getMappingConfig, isEmpty, makeDate, mapValidationIssues, transformHeaderRow } from './utils';
+
+jest.mock('fs', () => ({
+  readFileSync: jest.fn().mockReturnValue(`[
+    {"VA-column": "Country", "Template-column": "Country of origin"},
+    {"VA-column": "Author", "Template-column": "Paper author"},
+    {"VA-column": "Full Name", "Template-column": "Name"}
+  ]`)
+}))
 
 describe('isEmpty', () => {
   it('returns false for non-empty object', () => {
@@ -43,5 +52,37 @@ describe('makeDate', () => {
   it('returns date if year is not null and month is not null', () => {
     expect(makeDate(1990, 2).getFullYear()).toBe(1990);
     expect(makeDate(1990, 2).getMonth()).toBe(2);
+  });
+});
+
+describe('getMappingConfig', () => {
+  it('gets mapping config', () => {
+    process.cwd = jest.fn().mockReturnValue('test')
+    const config = getMappingConfig('source', 'type')
+    expect(config).toEqual([
+      {"VA-column": "Country", "Template-column": "Country of origin"},
+      {"VA-column": "Author", "Template-column": "Paper author"},
+      {"VA-column": "Full Name", "Template-column": "Name"}
+    ])
+    expect(fs.readFileSync).toHaveBeenCalledWith("test/public/templates/source/type-mapping.json",
+      {"encoding": "utf8", "flag": "r"})
+  });
+});
+
+describe('transformHeaderRow', () => {
+  it('transforms header row', () => {
+    const csv = `ENL_ID,Initials,Paper author,Year,Report Type,Published,V Data,Country of origin,Name
+    405,J D,Charlwood,1997,report,no,yes,29434,,Kasim el Girba`
+    const transformedCsv = transformHeaderRow(csv, 'source', 'type');
+    expect(transformedCsv).toEqual(`ENL_ID,Initials,Author,Year,Report Type,Published,V Data,Country,Full Name
+    405,J D,Charlwood,1997,report,no,yes,29434,,Kasim el Girba`)
+  });
+});
+
+describe('mapValidationIssues', () => {
+  it('maps validaiton issues', () => {
+    const issues = [["Issue with Country", "Issue with Full Name"], ["Issue with Latitude"]]
+    const transformedIssues = mapValidationIssues('source', 'type', issues);
+    expect(transformedIssues).toEqual([["Issue with Country of origin", "Issue with Name"], ["Issue with Latitude"]])
   });
 });
