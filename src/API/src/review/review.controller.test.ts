4a030f2a58a8a3a92cfb503d79adfe57179cef0a
andykitch <andrew.kitchener@capgemini.com>
2023-02-06 14:37:42 +0000

    Added test for controllwe method

--- src/API/src/review/review.controller.test.ts
+++ src/API/src/review/review.controller.test.ts
@@ -1,4 +1,6 @@
+import { AuthGuard } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import { RolesGuard } from 'src/auth/user_role/roles.guard';
 import { ReviewController } from './review.controller';
 
 describe('ReviewController', () => {
@@ -15,4 +17,12 @@ describe('ReviewController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('approveDataset', () => {
+    it('has guards applied', () => {
+      const guards = Reflect.getMetadata('__guards__', controller.approveDataset);
+      expect(guards[0]).toBe(AuthGuard('va'));
+      expect(guards[1]).toBe(RolesGuard);
+    })
+  })
 });
