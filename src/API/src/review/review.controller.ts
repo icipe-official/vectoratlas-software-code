42976d0574be8511cd9a59bd426c8933e21394a6
shishi5089 <shishinangs83@gmail.com>
2023-02-06 09:22:03 +0300

    Issue with start dev

--- src/API/src/review/review.controller.ts
+++ src/API/src/review/review.controller.ts
@@ -12,37 +12,19 @@ export class ReviewController {
     constructor(
         private reviewService: ReviewService,
         private readonly mailerService: MailerService,) {}
-/* @UseGuards(AuthGuard('va'), RolesGuard)
-@Roles(Role.Reviewer) */
+
 @Post('review')
 async reviewCsv(
   @AuthUser() user: any,
   @Query('datasetId') datasetId?: string,
-  //@Body('reviewFeedback')reviewFeedback?:string
+
 ){
-  //const userId = user.sub;
+
   if (datasetId) {
-/*     if (!(await this.ingestService.validDataset(datasetId))) {
-      throw new HttpException('No dataset exists with this id.', 403);
-    } */
     await this.reviewService.reviewDataset(
         datasetId
     )
     }
-/*   const response = `<div>
-  <h2>Reviewer Feedback</h2>
-  <p>Your file was reviewed. Please visit http://www.vectoratlas.icipe.org/review?dataset=${datasetId}</p>
-  </div>`;
-   this.mailerService.sendMail({
-    to: process.env.UPLOADER,
-    from: 'reviewer@email.com',
-    subject: 'Reviewer Feedback',
-    context: {
-        reviewFeedback : reviewFeedback
-    },
-  })
-
-      return response; */
   }
 
 }
--- src/API/src/review/review.service.ts
+++ src/API/src/review/review.service.ts
@@ -13,7 +13,8 @@ export const getAuth0Token = async (http: HttpService) => {
       client_id: 'sQPoZzmH4QaAHVEJrDaK3pPeHG0SmCtr',
       client_secret: 'u6gsaL3kmxHPSmAYVXoJxaD5gbz0x3WhyRZQAzY7calz0y40rwmP2wOBbzigVTOt',
       audience: 'https://dev-326tk4zu.us.auth0.com/api/v2/'
-    }, {headers: {'content-type': 'application/x-www-form-urlencoded', "Accept-Encoding": "gzip,deflate,compress"}}).pipe(
+    }, {headers: {'content-type': 'application/x-www-form-urlencoded',
+       "Accept-Encoding": "gzip,deflate,compress"}}).pipe(
       map((res: any) => {
         return res.data.access_token;
       }),
@@ -49,7 +50,9 @@ export class ReviewService {
            const userId = dataset.UpdatedBy;
 
            const emailAddress = await lastValueFrom(
-            this.httpService.get(`https://dev-326tk4zu.us.auth0.com/api/v2/users/${userId}`, {headers: {authorization: `Bearer ${token}`, "Accept-Encoding": "gzip,deflate,compress"}}).pipe(
+            this.httpService.get(`https://dev-326tk4zu.us.auth0.com/api/v2/users/${userId}`, 
+            {headers: {authorization: `Bearer ${token}`, "Accept-Encoding": "gzip,deflate,compress"}})
+            .pipe(
               map((res: any) => {
                 return res.data.email;
               }),
