82a14d66e01bb9de40852c3bc4307e6752e8ab82
csandbach <chris.sandbach@capgemini.com>
2023-02-06 16:40:31 +0000

    upform linting

--- src/UI/components/upload/data/Upform.test.tsx
+++ src/UI/components/upload/data/Upform.test.tsx
@@ -63,7 +63,11 @@ describe('ModelUpload', () => {
 
   it('calls action on upload click with valid inputs', async () => {
     const state: Partial<AppState> = {
-      upload: { ...initialState, dataFile: 'file', templateList: ['Vector Atlas']},
+      upload: {
+        ...initialState,
+        dataFile: 'file',
+        templateList: ['Vector Atlas'],
+      },
     };
     const { store, wrapper } = render(<Upform />, state);
 
@@ -90,7 +94,12 @@ describe('ModelUpload', () => {
 
   it('displays spinner when loading', () => {
     const state: Partial<AppState> = {
-      upload: { ...initialState, modelFile: 'file', loading: true,  templateList: ['Vector Atlas'] },
+      upload: {
+        ...initialState,
+        modelFile: 'file',
+        loading: true,
+        templateList: ['Vector Atlas'],
+      },
     };
     const { wrapper } = render(<Upform />, state);
     expect(wrapper.getByText('Circular progress mock')).toBeInTheDocument();
