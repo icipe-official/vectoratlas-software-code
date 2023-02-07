23ec2c7bc89c8e02b5b3668e116798d4d5e80209
shishi5089 <shishinangs83@gmail.com>
2023-02-07 10:46:51 +0300

    Unit tests

--- src/API/src/review/review.controller.test.ts
+++ src/API/src/review/review.controller.test.ts
@@ -1,12 +1,25 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Test, TestingModule } from '@nestjs/testing';
+import { MockType } from 'src/mocks';
 import { ReviewController } from './review.controller';
+import { ReviewService } from './review.service';
 
 describe('ReviewController', () => {
   let controller: ReviewController;
+  let reviewService: MockType<ReviewService>;
 
   beforeEach(async () => {
+    reviewService = {
+      reviewDataset: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReviewController],
+      providers: [
+        {
+          provide: ReviewService,
+          useValue: reviewService,
+        },
+      ]
     }).compile();
 
     controller = module.get<ReviewController>(ReviewController);
--- src/API/src/review/review.service.test.ts
+++ src/API/src/review/review.service.test.ts
@@ -1,14 +1,31 @@
+import { HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Dataset } from 'src/db/shared/entities/dataset.entity';
 import { MockType, repositoryMockFactory } from 'src/mocks';
 import { Repository } from 'typeorm';
-import { ReviewService } from './review.service';
+import { getAuth0Token, ReviewService } from './review.service';
+import * as rxjs from 'rxjs';
 
-jest.mock;
+jest.mock('@nestjs/axios', () => ({
+  HttpService: {
+    get: jest.fn(),
+    post: jest.fn(),
+  },
+}));
+
+jest.mock('rxjs', () => ({
+  lastValueFrom: jest
+    .fn((s) => s)
+    .mockResolvedValue([
+      { data: {access_token: 'testtoken', email:'testemail'} },
+    ]),
+  map: jest.fn(),
+}));
 
 describe('ReviewService', () => {
   let service: ReviewService;
+  let httpClient: HttpService;
   let datasetRepositoryMock: MockType<Repository<Dataset>>;
 
   beforeEach(async () => {
@@ -21,22 +38,38 @@ describe('ReviewService', () => {
       ],
       
     }).compile();
-
+    httpClient = module.get<HttpService>(HttpService);
     service = module.get<ReviewService>(ReviewService);
     datasetRepositoryMock = module.get(getRepositoryToken(Dataset));
+    process.env = {
+      REVIEWER_EMAIL_LIST: 'reviewers@gmail.com'
+    }
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
+  it('getAuth0Token should return a token', async () => {
+    expect(await getAuth0Token(httpClient)).toEqual([
+      {data: {access_token: 'testtoken', email:'testemail'} },
+    ]);
+  });
+
+  it('should send off a post request, from within getAuth0 token', async () => {
+    expect(httpClient.post).toHaveBeenCalledWith(
+      'https://dev-326tk4zu.us.auth0.com/oauth/token',
+    );
+    expect(rxjs.lastValueFrom).toHaveBeenCalled();
+  });
+
   describe('reviewDataset', () => {
-    fit('updates status of dataset to in review',async () => {
+    it('updates status of dataset to in review',async () => {
       await service.reviewDataset('example_id');
       expect(datasetRepositoryMock.update).toHaveBeenCalledTimes(1);
     });
 
-    fit('fails to update status', async () => {
+    it('fails to update status', async () => {
       datasetRepositoryMock.save = jest.fn().mockRejectedValue('ERROR');
 
       await expect(
