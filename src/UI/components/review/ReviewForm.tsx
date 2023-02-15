fa44a6d272f3a95c11f5eb86f2b33668fecbe6cc
andykitch <andrew.kitchener@capgemini.com>
2023-02-15 10:04:46 +0000

    Added loading behaviour

--- src/UI/components/review/ReviewForm.tsx
+++ src/UI/components/review/ReviewForm.tsx
@@ -42,6 +42,7 @@ function ReviewForm({ datasetId }: { datasetId: string }) {
     (state) => state.review.datasetMetadata
   );
   const loading = useAppSelector((state) => state.review.loading);
+  const downloading = useAppSelector((state) => state.review.downloading);
 
   const approveDatasetClick = () => {
     dispatch(approveDataset({ datasetId }));
@@ -136,6 +137,7 @@ function ReviewForm({ datasetId }: { datasetId: string }) {
                   variant="contained"
                   data-testid="dataDownload"
                   onClick={download_data}
+                  disabled={downloading}
                 >
                   Download data
                 </Button>
--- src/UI/state/review/actions/downloadData.ts
+++ src/UI/state/review/actions/downloadData.ts
@@ -1,17 +1,25 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import FileSaver from 'file-saver';
+import { toast } from 'react-toastify';
 import { getDatasetData } from '../../../api/api';
+import { setDownloading } from '../reviewSlice';
 
 export const downloadDatasetData = createAsyncThunk(
   'review/downloadDatasetData',
   async ({ datasetId }: { datasetId: string }, { dispatch }) => {
-    //dispatch(setLoading(true));
-    const data = await getDatasetData(datasetId);
+    dispatch(setDownloading(true));
+    try {
+      const data = await getDatasetData(datasetId);
 
-    var file = new Blob([data.data], {
-      type: 'text/csv;charset=utf-8',
-    });
+      var file = new Blob([data.data], {
+        type: 'text/csv;charset=utf-8',
+      });
 
-    FileSaver.saveAs(file, `data-${datasetId}.csv`);
+      FileSaver.saveAs(file, `data-${datasetId}.csv`);
+      dispatch(setDownloading(false));
+    } catch (e) {
+      toast.error('Something went wrong with data download. Please try again.')
+      dispatch(setDownloading(false));
+    }
   }
 );
