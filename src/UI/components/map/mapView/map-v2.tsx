a012e18cc52ef48d024e6c700d32f01b02fd7c30
Mandela M Muithi <mandelamitau72@gmail.com>
2023-02-23 16:41:24 +0300

    implement feature to zoom to extent of one or more countries that is selected in the 'country' filter

--- src/UI/components/map/mapView/map-v2.tsx
+++ src/UI/components/map/mapView/map-v2.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../state/hooks';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import { transform } from 'ol/proj';
+import { transform, transformExtent } from 'ol/proj';
 import Box from '@mui/material/Box';
 import DrawerMap from '../layers/drawerMap';
 import DataDrawer from '../layers/dataDrawer';
@@ -27,7 +27,11 @@ import {
 } from './pointUtils';
 import { registerDownloadHandler } from './downloadImageHandler';
 import { Typography } from '@mui/material';
-import ScaleLegend from './scaleLegend';
+import ScaleLegend from './scaleLegend';import * as african_countries_extents_array from '../utils/african_country_extents.json';
+import { extend, Extent } from 'ol/extent';
+import { getCombinedExtent, matchObjectKeys } from '../utils/zoomToFeatureUtil';
+
+
 
 const getNewColor = () => {
   const r = Math.floor(Math.random() * 255);
@@ -170,6 +174,16 @@ export const MapWrapperV2 = () => {
 
     updateSelectedPolygons(map, filters.areaCoordinates);
   }, [map, filters.areaCoordinates]);
+  
+  useEffect(()=>{
+    interface African_countries_extents {
+      [key: string]: number[];
+    }
+    const african_countries_extents: African_countries_extents = african_countries_extents_array;
+    if(filters.country.value.length>0 && typeof filters.country.value != 'string'){
+   map?.getView().fit(transformExtent(getCombinedExtent(matchObjectKeys( filters.country.value.map((current_country)=>(current_country.replace(/\s/g, ""))),african_countries_extents)), 'EPSG:4326', 'EPSG:3857'),{duration: 700, padding: [50, 50, 50, 50]})
+  }  
+  },[filters.country])
 
   return (
     <Box sx={{ display: 'flex', flexGrow: 1 }}>
---
+++ src/UI/components/map/utils/zoomToFeatureUtil.ts
@@ -0,0 +1,30 @@
+interface African_countries_extents {
+    [key: string]: number[];
+  }
+ export function getCombinedExtent(extents: number[][]| string): number[] {
+    if (extents.length === 0) {
+      return [];
+    }
+  if (typeof extents==='string') return [];
+
+    const minX = Math.min(...extents.map(extent => extent[0]));
+    const minY = Math.min(...extents.map(extent => extent[1]));
+    const maxX = Math.max(...extents.map(extent => extent[2]));
+    const maxY = Math.max(...extents.map(extent => extent[3]));
+  
+    return [minX, minY, maxX, maxY];
+  }
+
+ export function matchObjectKeys(search: any[], obj:African_countries_extents): number[][] {
+    const matchingValues: number[][] = [];
+    const keys = Object.keys(obj);
+    for (const key of keys) {
+      for (const term of search) {
+        if (key.toLowerCase( ).includes(term.toLowerCase( ))) {
+          matchingValues.push(obj[key]);
+          break;
+        }
+      }
+    }
+    return matchingValues;
+  }
