f884cb6c4960ea7e893df12e4af342ac6e0d8639
Steve Nyaga <stevenyaga@gmail.com>
2024-09-05 04:22:42 +0300

    feat: Extend data download to include additional fields in the occurrence entity. Also include fields of entities related to Occurrence entity at top level

--- src/API/src/db/occurrence/occurrence.resolver.ts
+++ src/API/src/db/occurrence/occurrence.resolver.ts
@@ -144,21 +144,95 @@ export class OccurrenceResolver {
     @Args({ name: 'bounds', type: () => BoundsFilter, nullable: true })
     bounds?: BoundsFilter,
     recordDownload?: boolean,
+    minimalFields = true,
   ) {
     const { items, total } = await this.occurrenceService.findOccurrences(
       take,
       skip,
       filters,
       bounds,
+      minimalFields,
     );
     if (recordDownload) {
       await this.occurrenceService.incrementDownload(items);
     }
-    const returnItems: OccurrenceReturn[] = items.map((x) => ({
-      id: x.id,
-      species: x.recordedSpecies.species,
-      location: x.site.location,
-    }));
+
+    const relationObject = this.occurrenceService.getOccurrenceFields(true);
+    const excludeColumns = {
+      parent: [],
+      relations: {
+        dataset: '*',
+        site: ['longitude_4', 'longitude_5'],
+      },
+    };
+
+    const includeColumn = (
+      isParentProperty: boolean,
+      relationName: string,
+      columnName: string,
+    ) => {
+      let cols: any;
+      if (isParentProperty) {
+        cols = excludeColumns['parent'];
+      } else {
+        cols = excludeColumns['relations'][relationName];
+      }
+
+      if (cols === '*') return false;
+      if (Array.isArray(cols) && cols.includes(columnName)) return false;
+      return true;
+    };
+
+    /**
+     * Include other fields in addition to those specified in the Interface
+     * extend to other relations. This contradicts strict typing requirements of OccurrenceReturn but it
+     * was necessary so that we allow inclusion of related fields dynamically
+     * extended fields will be renamed to `relationName_relationFieldName`
+     */
+    const selectAllFields = (record: object, destinationObject: object) => {
+      Object.keys(record).map((dataProperty) => {
+        // check if fld is a relation. If yes, loop through all fields for the relation
+        if (Object.keys(relationObject).includes(dataProperty)) {
+          const relationFields = relationObject[dataProperty];
+          relationFields.map((relationField) => {
+            if (
+              relationField === 'id' ||
+              !includeColumn(false, dataProperty, relationField)
+            ) {
+              // do nothing since field should not be included
+            } else {
+              const key = `${dataProperty}_${relationField}`;
+              Object.assign(destinationObject, {
+                [key]: record?.[dataProperty]?.[relationField] || null,
+              });
+            }
+          });
+        } else {
+          if (includeColumn(true, null, dataProperty)) {
+            Object.assign(destinationObject, {
+              [dataProperty]: record?.[dataProperty],
+            });
+          }
+        }
+      });
+      return destinationObject;
+    };
+
+    const returnItems: OccurrenceReturn[] = items.map((x) => {
+      const obj = {
+        id: x.id,
+        species: x.recordedSpecies.species,
+        location: x.site.location,
+      };
+
+      // extend to other relations. This contradicts strict typing requirements but it
+      // was necessary so that we allow inclusion of related fields dynamically
+      if (!minimalFields) {
+        const extendedObject = selectAllFields(x, obj);
+        Object.assign(obj, extendedObject);
+      }
+      return obj;
+    });
     return Object.assign(new PaginatedOccurrenceData(), {
       items: returnItems,
       total,
@@ -184,12 +258,31 @@ export class OccurrenceResolver {
       filters,
       bounds,
       true,
+      false, //set to false in order to load all data including relations
     );
     const flattenedRepoObject = flattenOccurrenceRepoObject(pageOfData.items);
     const headers = Object.keys(flattenedRepoObject[0]).join(',');
-    const csvRows = flattenedRepoObject.map((row) =>
-      Object.values(row).join(','),
-    );
+
+    /**
+     * Format csv rows to ensure correct export and display
+     * For column values containing a comma, enclose the value with double quote
+     */
+    const formatRow = (row: object) => {
+      const vals = Object.values(row).map((val: object) => {
+        let strVal = val?.toString();
+        const hasComma = strVal?.includes(',');
+        //val?.toString().replace(/,/g, '');// replace comma
+        strVal = strVal?.replace(/\"/g, '""'); //escape double quotes
+        return hasComma ? '"' + strVal + '"' : strVal; //enclose string if it has a comma
+      });
+      return vals.join(',');
+    };
+
+    const csvRows = flattenedRepoObject.map((row) => {
+      return formatRow(row);
+      // return Object.values(row).join(','),
+    });
+
     return Object.assign(new PaginatedStringData(), {
       items: [headers, ...csvRows],
       total: pageOfData.total,
