1540ea1c143c689a6b0ec2d6a9e710bf738e2c31
Mandela Muithi <mandelamitau72@gmail.com>
2025-01-06 11:24:29 +0300

    correct url for fetching doi data from api

--- src/UI/components/map/mapView/map-v2.tsx
+++ src/UI/components/map/mapView/map-v2.tsx
@@ -130,7 +130,7 @@ export const MapWrapperV2 = ({ doi }: { doi?: string } = {}) => {
       try {
         if (doi) {
           // Fetch filters from the API if DOI is provided
-          const response = await fetch(`http://localhost:3001/doi?doi=${doi}`);
+          const response = await fetch(`/vector-api/doi?doi=${doi}`);
           const data = await response.json();
           const fetchedFilters = data[0]?.meta_data?.filters;
 
