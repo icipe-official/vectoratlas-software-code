0b97e4bfdf587f9887a9845597ab846573fa8016
Steve Nyaga <stevenyaga@gmail.com>
2025-02-24 18:38:07 +0300

    db: make creator_email and creator_name nullable fields

---
+++ src/API/src/db/migrations/1740394233594-remove-creator-email-creator-name-as-mandatory.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class RemoveCreatorEmailCreatorNameAsMandatory1740394233594 implements MigrationInterface {
+    name = 'RemoveCreatorEmailCreatorNameAsMandatory1740394233594'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "doi" ALTER COLUMN "creator_name" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "doi" ALTER COLUMN "creator_email" DROP NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "doi" ALTER COLUMN "creator_email" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "doi" ALTER COLUMN "creator_name" SET NOT NULL`);
+    }
+
+}
