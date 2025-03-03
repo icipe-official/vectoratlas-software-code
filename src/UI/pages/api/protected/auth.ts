41aed726a9ec8ade6b0c112763d81d6831fb9ef3
stevenyaga <stevenyaga@gmail.com>
2025-03-03 10:46:16 +0300

    Update auth.ts

--- src/UI/pages/api/protected/auth.ts
+++ src/UI/pages/api/protected/auth.ts
@@ -4,7 +4,7 @@ import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 export default withApiAuthRequired(async function ProtectedRoute(req, res) {
   const session = await getSession(req, res);
   try {
-    alert(`Auth endpoint: ${process.env.NEXT_PUBLIC_AUTH_ENDPOINT}`);
+    console.log(`Auth endpoint: ${process.env.NEXT_PUBLIC_AUTH_ENDPOINT}`);
     const tokenResponse = await axios.get(
       process.env.NEXT_PUBLIC_AUTH_ENDPOINT ?? '',
       // 'http://api:3001/auth/token',
