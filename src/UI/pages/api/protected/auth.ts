01981a5aa4f0a2fb4e3ee4227f2d1de35fa8f2e5
stevenyaga <stevenyaga@gmail.com>
2025-03-03 13:13:03 +0300

    Update auth.ts

--- src/UI/pages/api/protected/auth.ts
+++ src/UI/pages/api/protected/auth.ts
@@ -8,14 +8,12 @@ export default withApiAuthRequired(async function ProtectedRoute(req, res) {
     console.log(`Auth session: ${session?.accessToken}`);
     const url = '/vector-api/auth/token'; // process.env.NEXT_PUBLIC_AUTH_ENDPOINT ?? '', 'http://api:3001/auth/token'
     console.log(`Auth url: ${url}`);
-    const tokenResponse = await axios.get(url,
-      {
-        withCredentials: true,
-        headers: {
-          Authorization: 'Bearer ' + session?.accessToken,
-        },
-      }
-    );
+    const tokenResponse = await axios.get(url, {
+      withCredentials: true,
+      headers: {
+        Authorization: 'Bearer ' + session?.accessToken,
+      },
+    });
     console.log('token response', tokenResponse);
 
     res.status(200).json(tokenResponse.data);
