804c4ce23ffdb1c2562b1e159a09c1e9ef3fbbe8
stevenyaga <stevenyaga@gmail.com>
2025-03-05 05:48:09 +0300

    Update auth.ts

--- src/UI/pages/api/protected/auth.ts
+++ src/UI/pages/api/protected/auth.ts
@@ -1,8 +1,48 @@
-'use client';
-
 import axios from 'axios';
 import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 
+export default withApiAuthRequired(async function ProtectedRoute(req, res) {
+  const session = await getSession(req, res);
+  const tokenResponse = await fetch(
+    process.env.NEXT_PUBLIC_AUTH_ENDPOINT ?? '',
+    {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        Authorization: 'Bearer ' + session?.accessToken,
+      },
+    }
+  );
+  const token = await tokenResponse.text();
+  res.status(200).json(token);
+});
+
+/*
+export default withApiAuthRequired(async function ProtectedRoute(req, res) {
+  const session = await getSession(req, res);
+  try {
+    const url = process.env.NEXT_PUBLIC_AUTH_ENDPOINT ?? '';
+    const tokenResponse = await axios.get(url, {
+      withCredentials: true,
+      headers: {
+        Authorization: 'Bearer ' + session?.accessToken,
+      },
+    });
+    console.log('token response', tokenResponse);
+
+    res.status(200).json(tokenResponse.data);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: 'An unknown error occurred' });
+    }
+  }
+});
+
+*/
+
+/*
 export default async function ProtectedRoute(req: any, res: any) {
   console.log('Calling auth/protected, ', req);
   const session = await getSession(req, res);
@@ -19,45 +59,4 @@ export default async function ProtectedRoute(req: any, res: any) {
   const token = await tokenResponse.text();
   res.status(200).json(token);
 }
-/*
-export default withApiAuthRequired(async function ProtectedRoute(req, res) {
-  const session = await getSession(req, res);
-  const tokenResponse = await fetch(
-    process.env.NEXT_PUBLIC_AUTH_ENDPOINT ?? '',
-    {
-      method: 'GET',
-      credentials: 'include',
-      headers: {
-        Authorization: 'Bearer ' + session?.accessToken,
-      },
-    }
-  );
-  const token = await tokenResponse.text();
-  res.status(200).json(token);
-});
 */
-
-// export default withApiAuthRequired(async function ProtectedRoute(req, res) {
-//   const session = await getSession(req, res);
-//   try {
-//     // console.log(`Auth endpoint: ${process.env.NEXT_PUBLIC_AUTH_ENDPOINT}`);
-//     // console.log(`Auth session: ${session?.accessToken}`);
-//     const url = '/vector-api/auth/token'; // process.env.NEXT_PUBLIC_AUTH_ENDPOINT ?? '', 'http://api:3001/auth/token'
-//     // console.log(`Auth url: ${url}`);
-//     const tokenResponse = await axios.get(url, {
-//       withCredentials: true,
-//       headers: {
-//         Authorization: 'Bearer ' + session?.accessToken,
-//       },
-//     });
-//     console.log('token response', tokenResponse);
-
-//     res.status(200).json(tokenResponse.data);
-//   } catch (error: unknown) {
-//     if (error instanceof Error) {
-//       res.status(500).json({ error: error.message });
-//     } else {
-//       res.status(500).json({ error: 'An unknown error occurred' });
-//     }
-//   }
-// });
