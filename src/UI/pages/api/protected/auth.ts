9e76bcfe188470975062d93184c190726170c632
stevenyaga <stevenyaga@gmail.com>
2025-03-05 05:28:19 +0300

    Update auth.ts

--- src/UI/pages/api/protected/auth.ts
+++ src/UI/pages/api/protected/auth.ts
@@ -1,6 +1,25 @@
+'use client';
+
 import axios from 'axios';
 import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 
+export default async function ProtectedRoute(req, res) {
+  console.log('Calling auth/protected, ', req);
+  const session = await getSession(req, res);
+  const tokenResponse = await fetch(
+    process.env.NEXT_PUBLIC_AUTH_ENDPOINT ?? '',
+    {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        Authorization: 'Bearer ' + session?.accessToken,
+      },
+    }
+  );
+  const token = await tokenResponse.text();
+  res.status(200).json(token);
+}
+/*
 export default withApiAuthRequired(async function ProtectedRoute(req, res) {
   const session = await getSession(req, res);
   const tokenResponse = await fetch(
@@ -13,10 +32,10 @@ export default withApiAuthRequired(async function ProtectedRoute(req, res) {
       },
     }
   );
-  console.log('Access token', session?.accessToken);
   const token = await tokenResponse.text();
   res.status(200).json(token);
 });
+*/
 
 // export default withApiAuthRequired(async function ProtectedRoute(req, res) {
 //   const session = await getSession(req, res);
