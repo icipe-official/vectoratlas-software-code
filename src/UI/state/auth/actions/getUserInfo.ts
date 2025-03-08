6c25145ca4e70a59d12fd6c6aa0cb327dad79901
Steve Nyaga <stevenyaga@gmail.com>
2025-03-08 16:26:45 +0300

    refactor: Undo console.log debug statements

--- src/UI/state/auth/actions/getUserInfo.ts
+++ src/UI/state/auth/actions/getUserInfo.ts
@@ -3,19 +3,14 @@ import { fetchAuth } from '../../../api/api';
 import * as njwt from 'njwt';
 
 export const getUserInfo = createAsyncThunk('auth/getUserInfo', async () => {
-  debugger;
-  console.log('Getting user info');
   const token = await fetchAuth();
-  console.log('Retrieved token: ', token);
   const verifiedToken: any = njwt.verify(
     token,
     process.env.NEXT_PUBLIC_TOKEN_KEY
   );
-  console.log('Verified token: ', token);
   return {
     sub: verifiedToken?.body.sub,
     token: token,
-    // roles: ['uploader', 'admin', 'editor', 'reviewer', 'reviewer-manager'], // verifiedToken?.body.scope.split(','),
     roles: verifiedToken?.body.scope.split(','),
   };
 });
