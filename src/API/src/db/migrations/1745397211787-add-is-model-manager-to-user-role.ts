0a1c9579319483b4ab603d99406de9552e204e39
nyaga <Steve Nyaga>
2025-04-25 18:46:59 +0300

    migrations: Create migrations for uploaded models schema

---
+++ src/API/src/db/migrations/1745397211787-add-is-model-manager-to-user-role.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddIsModelManagerToUserRole1745397211787
+  implements MigrationInterface
+{
+  name = 'AddIsModelManagerToUserRole1745397211787';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user_role" ADD "is_model_manager" boolean`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user_role" DROP COLUMN "is_model_manager"`,
+    );
+  }
+}
