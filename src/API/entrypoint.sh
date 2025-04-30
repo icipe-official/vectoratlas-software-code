ecf81faf9a0db65fa0ea936a3bcc9a3906023ca4
Mandela Mitau <78897583+main-tech@users.noreply.github.com>
2025-04-30 15:25:53 +0300

    Update entrypoint.sh

--- src/API/entrypoint.sh
+++ src/API/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+set -x  # Enable debug mode
 
-# Ensure version.txt is copied to the mounted volume directory if it's missing
 if [ ! -f /usr/src/app/dist/public/version.txt ]; then
   echo "version.txt not found in mounted volume. Copying from /usr/src/app/version.txt"
   cp /usr/src/app/version.txt /usr/src/app/dist/public/version.txt
 fi
 
-# Start your application
 exec "$@"
