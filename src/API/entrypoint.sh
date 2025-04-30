4b6187ed3226b0a9c1d12a2297437db1b4317e0c
Mandela Mitau <78897583+main-tech@users.noreply.github.com>
2025-04-30 13:19:51 +0300

    Update entrypoint.sh

--- src/API/entrypoint.sh
+++ src/API/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Start the main application command
-"$@"
+# Ensure version.txt is copied to the mounted volume directory if it's missing
+if [ ! -f /usr/src/app/dist/public/version.txt ]; then
+  echo "version.txt not found in mounted volume. Copying from /usr/src/app/version.txt"
+  cp /usr/src/app/version.txt /usr/src/app/dist/public/version.txt
+fi
 
-# Overwrite version.txt with the one from the image
-cp /tmp/version.txt /usr/src/app/dist/public/version.txt
+# Start your application
+exec "$@"
