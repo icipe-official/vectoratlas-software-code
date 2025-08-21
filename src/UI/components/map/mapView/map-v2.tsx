a6d6fe880685d3916e6f7ad687885018e490b754
Lovestrant <kemboilovestrant@gmail.com>
2025-08-21 13:27:54 +0300

    Modified UI a bit

--- src/UI/components/map/mapView/map-v2.tsx
+++ src/UI/components/map/mapView/map-v2.tsx
@@ -356,7 +356,7 @@ export const MapWrapperV2 = ({
           right: 10,
           top: 200,
           zIndex: 10,
-          height: 200,
+          height: 201,
           color: 'black',
         }}
       >
