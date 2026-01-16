66e4e52b9503f3d6896753af89aa2ee5518e754f
Lucy okoth <lucyakokoth@gmail.com>
2026-01-16 12:58:05 +0300

    style(about-team): fix prettier formatting

--- src/UI/components/about/aboutTeam.tsx
+++ src/UI/components/about/aboutTeam.tsx
@@ -26,26 +26,25 @@ export default function AboutTeam() {
   const [selectedTeamMember, setSelectedTeamMember] = useState<any>(undefined);
 
   useEffect(() => {
-  const loadTeam = async () => {
-    let team;
+    const loadTeam = async () => {
+      let team;
 
-    switch (locale) {
-      case 'fr':
-        team = (await import('./data/team-fr.json')).default;
-        break;
-      case 'pt':
-        team = (await import('./data/team-pt.json')).default;
-        break;
-      default:
-        team = (await import('./data/team-en.json')).default;
-    }
+      switch (locale) {
+        case 'fr':
+          team = (await import('./data/team-fr.json')).default;
+          break;
+        case 'pt':
+          team = (await import('./data/team-pt.json')).default;
+          break;
+        default:
+          team = (await import('./data/team-en.json')).default;
+      }
 
-    setTeamMembers(team?.teamList || []);
-  };
+      setTeamMembers(team?.teamList || []);
+    };
 
-  loadTeam();
-}, [locale]);
-
+    loadTeam();
+  }, [locale]);
 
   const handleClose = () => setSelectedTeamMember(undefined);
 
