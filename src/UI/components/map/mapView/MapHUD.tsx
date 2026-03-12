c8709fbf0a31a041dc88b008d2f7701f203eb73a
Mandela Muithi <mandelamitau72@gmail.com>
2026-03-12 15:16:37 +0300

    run lint fix

--- src/UI/components/map/mapView/MapHUD.tsx
+++ src/UI/components/map/mapView/MapHUD.tsx
@@ -211,8 +211,8 @@ const MapHUD: React.FC<MapHUDProps> = ({
       active && payload?.length
         ? payload[0].payload
         : touchedSpecies
-          ? donutData.find((d) => d.name === touchedSpecies)
-          : null;
+        ? donutData.find((d) => d.name === touchedSpecies)
+        : null;
 
     if (!data) return null;
 
@@ -406,8 +406,8 @@ const MapHUD: React.FC<MapHUDProps> = ({
                 normalizedSp === OTHER_LABEL
                   ? { color: '#038543' }
                   : speciesStyles.find(
-                    (s) => normalize(s.species) === normalizedSp
-                  );
+                      (s) => normalize(s.species) === normalizedSp
+                    );
               const isHovered = hoveredSpecies === normalizedSp;
 
               return (
@@ -441,8 +441,9 @@ const MapHUD: React.FC<MapHUDProps> = ({
                         ? 'rgba(26, 31, 36)'
                         : 'rgba(15, 20, 25, 0.95)',
                       backdropFilter: 'blur(8px)',
-                      border: `1px solid ${style?.color ?? 'rgba(255,255,255,0.1)'
-                        }`,
+                      border: `1px solid ${
+                        style?.color ?? 'rgba(255,255,255,0.1)'
+                      }`,
                     }}
                   >
                     {/* ENERGY BAR */}
@@ -452,8 +453,9 @@ const MapHUD: React.FC<MapHUDProps> = ({
                         left: 0,
                         top: 0,
                         bottom: 0,
-                        width: `${(count / Math.max(totalFilteredPoints, 1)) * 100
-                          }%`,
+                        width: `${
+                          (count / Math.max(totalFilteredPoints, 1)) * 100
+                        }%`,
                         background: `linear-gradient(90deg, ${style?.color}, transparent)`,
                         opacity: 0.25,
                       }}
@@ -560,7 +562,7 @@ const MapHUD: React.FC<MapHUDProps> = ({
                             }}
                           >
                             <Typography fontSize={11} fontStyle="italic">
-                              An.  {usp}
+                              An. {usp}
                             </Typography>
                             <Typography
                               fontSize={11}
