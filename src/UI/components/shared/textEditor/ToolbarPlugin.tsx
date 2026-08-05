59431fd63e638aa4e9391dc14a2fca6c0caf3fe0
IndianaEunice <euniceomuhindi@gmail.com>
2026-08-05 13:29:05 +0300

    update fix

--- src/UI/components/shared/textEditor/ToolbarPlugin.tsx
+++ src/UI/components/shared/textEditor/ToolbarPlugin.tsx
@@ -37,7 +37,11 @@ import { useTranslations } from 'next-intl';
 
 const LowPriority = 1;
 
-const EditorToolbar = () => {
+const EditorToolbar = ({
+  hideBlockTypeSelector,
+}: {
+  hideBlockTypeSelector?: boolean;
+}) => {
   const t = useTranslations('RichTextEditor');
   const [editor] = useLexicalComposerContext();
   const [blockType, setBlockType] = useState('paragraph');
@@ -139,19 +143,21 @@ const EditorToolbar = () => {
 
   return (
     <Toolbar variant="dense" disableGutters>
-      <FormControl sx={{ minWidth: 120 }} size="small">
-        <Select
-          value={blockType}
-          onChange={handleBlockTypeChange}
-          sx={{ minWidth: '200px' }}
-        >
-          <MenuItem value={'paragraph'}>{t('normal')}</MenuItem>
-          <MenuItem value={'h1'}>{t('largeHeading')}</MenuItem>
-          <MenuItem value={'h2'}>{t('smallHeading')}</MenuItem>
-          <MenuItem value={'ul'}>{t('bulletList')}</MenuItem>
-          <MenuItem value={'ol'}>{t('numberedList')}</MenuItem>
-        </Select>
-      </FormControl>
+      {!hideBlockTypeSelector && (
+        <FormControl sx={{ minWidth: 120 }} size="small">
+          <Select
+            value={blockType}
+            onChange={handleBlockTypeChange}
+            sx={{ minWidth: '200px' }}
+          >
+            <MenuItem value={'paragraph'}>{t('normal')}</MenuItem>
+            <MenuItem value={'h1'}>{t('largeHeading')}</MenuItem>
+            <MenuItem value={'h2'}>{t('smallHeading')}</MenuItem>
+            <MenuItem value={'ul'}>{t('bulletList')}</MenuItem>
+            <MenuItem value={'ol'}>{t('numberedList')}</MenuItem>
+          </Select>
+        </FormControl>
+      )}
       <ToggleButtonGroup
         value={formats}
         onChange={handleFormat}
@@ -173,8 +179,8 @@ const EditorToolbar = () => {
           <FormatItalicIcon />
         </ToggleButton>
         <ToggleButton
-          value="underlined"
-          aria-label="underlined"
+          value="underline"
+          aria-label="underline"
           onClick={toggleFormat('underline')}
         >
           <FormatUnderlinedIcon />
