f36067a19b2279700d91fb6947d0cc3dcc0216da
Lucy okoth <lucyakokoth@gmail.com>
2026-08-25 12:23:07 +0300

    Migrate Help site from Docusaurus to Sphinx + sphinx_rtd_theme

---
+++ src/Help/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+project = 'Vector Atlas Help'
+copyright = '2026, ICIPE'
+author = 'ICIPE Vector Atlas Team'
+release = '1.0'
+
+extensions = [
+    'sphinx_rtd_theme',
+    'myst_parser',
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+templates_path = ['_templates']
+
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+    'node_modules',
+    'venv',
+    'build',
+    'static',
+    'docs',
+    'README.md',
+    'TEST_BUILD_PUSH',
+]
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = ['custom.css']
+
+html_theme_options = {
+    'logo_only': False,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+}
+
+html_logo = '_static/vector-atlas-logo.svg'
+html_favicon = '_static/Animals-Mosquito-icon.png'
