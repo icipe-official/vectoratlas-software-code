e8d6a9b2b8ef18900962438d4b1ba3ce34c9fc4b
Charity <ann.karoki@students.jkuat.ac.ke>
2026-09-07 16:06:45 +0300

    fix prettier error

--- src/UI/components/map/layers/downloadFullDataControl.tsx
+++ src/UI/components/map/layers/downloadFullDataControl.tsx
@@ -46,8 +46,12 @@ export const DownloadFullDataControl = () => {
         setFirstName(parts[0] || '');
         setLastName(parts.slice(1).join(' ') || '');
       } else {
-        setFirstName(typeof user.given_name === 'string' ? user.given_name : '');
-        setLastName(typeof user.family_name === 'string' ? user.family_name : '');
+        setFirstName(
+          typeof user.given_name === 'string' ? user.given_name : ''
+        );
+        setLastName(
+          typeof user.family_name === 'string' ? user.family_name : ''
+        );
       }
       setEmail(user.email || '');
     }
